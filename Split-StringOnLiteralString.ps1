b185adc5144506f5a6069a36bc140711446663ae
Frank Lesniak <franklesniak@hotmail.com>
2020-05-22 22:58:44 -0500

    Uploading Split-StringOnLiteralString.ps1 and adding license to PSScriptAnalyzerSettings.ps1d

---
+++ Split-StringOnLiteralString.ps1
@@ -0,0 +1,78 @@
+# Split-StringOnLiteralString.ps1 contains one function (Split-StringOnLiteralString) that is
+# designed to split a string the way the way that I expected it to be done (as opposed to the
+# way that the -split operator does it). This function splits strings the way that VBScript's
+# Split function did it, and it's compatible with all versions of PowerShell.
+
+#region License
+###############################################################################################
+# Copyright 2020 Frank Lesniak
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###############################################################################################
+#endregion License
+
+function Split-StringOnLiteralString {
+    # This function takes two positional arguments
+    # The first argument is a string, and the string to be split
+    # The second argument is a string or char, and it is that which is to split the string in the first parameter
+    #
+    # Wrap this function call in "a cast to array" to ensure that it always returns an array even when the result is a single string.
+    # Example:
+    # $result = @(Split-StringOnLiteralString "foo" " ")
+    # # $result.GetType().FullName is System.Object[]
+    # # $result.Count is 1
+
+    trap {
+        Write-Error "An error occurred using the Split-StringOnLiteralString function. This was most likely caused by the arguments supplied not being strings"
+    }
+
+    if ($args.Length -ne 2) {
+        Write-Error "Split-StringOnLiteralString was called without supplying two arguments. The first argument should be the string to be split, and the second should be the string or character on which to split the string."
+    } else {
+        if ($null -eq $args[0]) {
+            # String to be split was $null; return an empty array. Leading comma ensures that
+            # PowerShell cooperates and returns the array as desired (without collapsing it)
+            ,@()
+        } elseif ($null -eq $args[1]) {
+            # Splitter was $null; return string to be split within an array (of one element).
+            # Leading comma ensures that PowerShell cooperates and returns the array as desired
+            # (without collapsing it
+            ,($args[0])
+        } else {
+            if (($args[0]).GetType().Name -ne "String") {
+                Write-Warning "The first argument supplied to Split-StringOnLiteralString was not a string. It will be attempted to be converted to a string. To avoid this warning, cast arguments to a string before calling Split-StringOnLiteralString."
+                $strToSplit = [string]$args[0]
+            } else {
+                $strToSplit = $args[0]
+            }
+
+            if ((($args[1]).GetType().Name -ne "String") -and (($args[1]).GetType().Name -ne "Char")) {
+                Write-Warning "The second argument supplied to Split-StringOnLiteralString was not a string. It will be attempted to be converted to a string. To avoid this warning, cast arguments to a string before calling Split-StringOnLiteralString."
+                $strSplitter = [string]$args[1]
+            } elseif (($args[1]).GetType().Name -eq "Char") {
+                $strSplitter = [string]$args[1]
+            } else {
+                $strSplitter = $args[1]
+            }
+
+            $strSplitterInRegEx = [regex]::Escape($strSplitter)
+
+            # With the leading comma, force encapsulation into an array so that an array is
+            # returned even when there is one element:
+            ,[regex]::Split($strToSplit, $strSplitterInRegEx)
+        }
+    }
+}
