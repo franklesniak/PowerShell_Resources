c19dc9aeeb763b4ec75c2be7f96c558481c0f182
Frank Lesniak <franklesniak@hotmail.com>
2020-08-18 21:58:46 -0500

    Code cleanup; add version and other metadata

--- Convert-IniToHashTable.ps1
+++ Convert-IniToHashTable.ps1
@@ -2,6 +2,8 @@
 # (key-value pairs). In doing so, the ini files can be accessed in a performant, hierarchical
 # manner that avoids slow string-parsing techniques.
 
+$strThisScriptVersionNumber = [version]'1.0.20200818.0'
+
 #region License
 ###############################################################################################
 # Copyright 2020 Frank Lesniak
@@ -23,13 +25,29 @@
 ###############################################################################################
 #endregion License
 
+#region DownloadLocationNotice
+# The most up-to-date version of this script can be found on the author's GitHub repository
+# at https://github.com/franklesniak/PowerShell_Resources
+#endregion DownloadLocationNotice
+
 function New-BackwardCompatibleCaseInsensitiveHashtable {
+    # New-BackwardCompatibleCaseInsensitiveHashtable is designed to create a case-insensitive
+    # hashtable that is backward-compatible all the way to PowerShell v1, yet forward-
+    # compatible to all versions of PowerShell. It replaces other constructors on newer
+    # versions of PowerShell such as:
+    # $hashtable = @{}
+    # This function is useful if you need to work with hashtables (key-value pairs), but also
+    # need your code to be able to run on any version of PowerShell.
+    #
     # Usage:
     # $hashtable = New-BackwardCompatibleCaseInsensitiveHashtable
+
+    $strThisFunctionVersionNumber = [version]'1.0.20200817.0'
+
     $cultureDoNotCare = [System.Globalization.CultureInfo]::InvariantCulture
-    $caseInsensitiveHashCodeProvider = New-Object -TypeName "System.Collections.CaseInsensitiveHashCodeProvider" -ArgumentList @($cultureDoNotCare)
-    $caseInsensitiveComparer = New-Object -TypeName "System.Collections.CaseInsensitiveComparer" -ArgumentList @($cultureDoNotCare)
-    New-Object -TypeName "System.Collections.Hashtable" -ArgumentList @($caseInsensitiveHashCodeProvider, $caseInsensitiveComparer)
+    $caseInsensitiveHashCodeProvider = New-Object -TypeName 'System.Collections.CaseInsensitiveHashCodeProvider' -ArgumentList @($cultureDoNotCare)
+    $caseInsensitiveComparer = New-Object -TypeName 'System.Collections.CaseInsensitiveComparer' -ArgumentList @($cultureDoNotCare)
+    New-Object -TypeName 'System.Collections.Hashtable' -ArgumentList @($caseInsensitiveHashCodeProvider, $caseInsensitiveComparer)
 }
 
 function Convert-IniToHashTable {
@@ -40,9 +58,9 @@ function Convert-IniToHashTable {
     # The first argument is a reference to an object that will be used to store output
     # The second argument is a string representing the file path to the ini file
     # The third argument is an array of characters that represent the characters allowed to
-    #   indicate the start of a comment. Usually, this should be set to @(";"), but if hashtags
+    #   indicate the start of a comment. Usually, this should be set to @(';'), but if hashtags
     #   are also allowed as comments for a given application, then it should be set to
-    #   @(";", "#") or @("#")
+    #   @(';', '#') or @('#')
     # The fourth argument is a boolean value that indicates whether comments should be ignored.
     #   Normally, comments should be ignored, and so this should be set to $true
     # The fifth argument is a boolean value that indicates whether comments must be on their
@@ -54,23 +72,23 @@ function Convert-IniToHashTable {
     #   value ; this text would not be considered a comment
     # The sixth argument is a string representation of the null section name. In other words,
     #   if a key-value pair is found outside of a section, what should be used as its fake
-    #   section name? As an example, this can be set to "NoSection" as long as their is no
+    #   section name? As an example, this can be set to 'NoSection' as long as their is no
     #   section in the ini file like [NoSection]
     # The seventh argument is a boolean value that indicates whether it is permitted for keys
     #   in the ini file to be supplied without an equal sign (if $true, the key is ingested but
     #   the value is regarded as $null). If set to false, lines that lack an equal sign are
     #   considered invalid and ignored.
     # If supplied, the eighth argument is a string representation of the comment prefix and is
-    #   to being the name of the "key" representing the comment (and appended with an index
+    #   to being the name of the 'key' representing the comment (and appended with an index
     #   number beginning with 1). If argument four is set to $false, then this argument is
-    #   required. Usually "Comment" is OK to use, unless there are keys in the file named like
-    #   "Comment1", "Comment2", etc.
+    #   required. Usually 'Comment' is OK to use, unless there are keys in the file named like
+    #   'Comment1', 'Comment2', etc.
     #
     # The function returns a 0 if successful, non-zero otherwise.
     #
     # Example usage:
     # $hashtableConfigIni = $null
-    # $intReturnCode = Convert-IniToHashTable ([ref]$hashtableConfigIni) ".\config.ini" @(";") $true $true "NoSection" $true
+    # $intReturnCode = Convert-IniToHashTable ([ref]$hashtableConfigIni) '.\config.ini' @(';') $true $true 'NoSection' $true
     #
     # This function is derived from Get-IniContent at the website:
     # https://github.com/lipkau/PsIni/blob/master/PSIni/Functions/Get-IniContent.ps1
@@ -110,19 +128,21 @@ function Convert-IniToHashTable {
         $strCommentPrefix = $args[7]
     }
 
+    $strThisFunctionVersionNumber = [version]'1.0.20200818.0'
+
     # Initialize regex matching patterns
     $arrCharCommentIndicator = $arrCharCommentIndicator | ForEach-Object {
         [regex]::Escape($_)
     }
-    $strRegexComment = "^\s*([$($arrCharCommentIndicator -join '')].*)$"
-    $strRegexCommentAnywhere = "\s*([$($arrCharCommentIndicator -join '')].*)$"
-    $strRegexSection = "^\s*\[(.+)\]\s*$"
-    $strRegexKey = "^\s*(.+?)\s*=\s*(['`"]?)(.*)\2\s*$"
+    $strRegexComment = '^\s*([' + ($arrCharCommentIndicator -join '') + '].*)$'
+    $strRegexCommentAnywhere = '\s*([' + ($arrCharCommentIndicator -join '') + '].*)$'
+    $strRegexSection = '^\s*\[(.+)\]\s*$'
+    $strRegexKey = '^\s*(.+?)\s*=\s*([''"]?)(.*)\2\s*$'
 
     $hashtableIni = New-BackwardCompatibleCaseInsensitiveHashtable
 
     if ((Test-Path $strFilePath) -eq $false) {
-        Write-Error ("Could not process INI file; the specified file was not found: " + $strFilePath)
+        Write-Error ('Could not process INI file; the specified file was not found: ' + $strFilePath)
         1 # return failure code
     } else {
         $intCommentCount = 0
@@ -149,7 +169,7 @@ function Convert-IniToHashTable {
                     }
                     $intCommentCount++
                     if (($hashtableIni.Item($strEffectiveSection)).ContainsKey($strCommentPrefix + ([string]$intCommentCount))) {
-                        Write-Warning ("File `"" + $strFilePath + "`", section `"" + $strEffectiveSection + "`" already unexpectedly contains a key `"" + ($strCommentPrefix + ([string]$intCommentCount)) + "`" with value `"" + ($hashtableIni.Item($strEffectiveSection)).Item($strCommentPrefix + ([string]$intCommentCount)) + "`". Key's value will be changed to: `"" + $Matches[1] + "`"")
+                        Write-Warning ('File "' + $strFilePath + '", section "' + $strEffectiveSection + '" already unexpectedly contains a key "' + ($strCommentPrefix + ([string]$intCommentCount)) + '" with value "' + ($hashtableIni.Item($strEffectiveSection)).Item($strCommentPrefix + ([string]$intCommentCount)) + '". Key''s value will be changed to: "' + $Matches[1] + '"')
                         ($hashtableIni.Item($strEffectiveSection)).Item($strCommentPrefix + ([string]$intCommentCount)) = $Matches[1]
                     } else {
                         ($hashtableIni.Item($strEffectiveSection)).Add($strCommentPrefix + ([string]$intCommentCount), $Matches[1])
@@ -180,7 +200,7 @@ function Convert-IniToHashTable {
                     } else {
                         # No key-value pair found
                         if ($boolAllowKeysWithoutValuesThatOmitEqualSign) {
-                            if (($null -ne $arrLine[0]) -and ("" -ne $arrLine[0])) {
+                            if (($null -ne $arrLine[0]) -and ($arrLine[0]) -ne '') {
                                 $strKey = $arrLine[0]
                             }
                         }
@@ -197,7 +217,7 @@ function Convert-IniToHashTable {
                     } else {
                         # No key-value pair found
                         if ($boolAllowKeysWithoutValuesThatOmitEqualSign) {
-                            if (($null -ne $arrLineKeyValue[0]) -and ("" -ne $arrLineKeyValue[0])) {
+                            if (($null -ne $arrLineKeyValue[0]) -and ($arrLineKeyValue[0]) -ne '') {
                                 $strKey = $arrLineKeyValue[0]
                             }
                         }
@@ -207,7 +227,7 @@ function Convert-IniToHashTable {
                         if ($boolIgnoreComments -ne $true) {
                             $intCommentCount++
                             if (($hashtableIni.Item($strEffectiveSection)).ContainsKey($strCommentPrefix + ([string]$intCommentCount))) {
-                                Write-Warning ("File `"" + $strFilePath + "`", section `"" + $strEffectiveSection + "`" already unexpectedly contains a key `"" + ($strCommentPrefix + ([string]$intCommentCount)) + "`" with value `"" + ($hashtableIni.Item($strEffectiveSection)).Item($strCommentPrefix + ([string]$intCommentCount)) + "`". Key's value will be changed to: `"" + $Matches[1] + "`"")
+                                Write-Warning ('File "' + $strFilePath + '", section "' + $strEffectiveSection + '" already unexpectedly contains a key "' + ($strCommentPrefix + ([string]$intCommentCount)) + '" with value "' + ($hashtableIni.Item($strEffectiveSection)).Item($strCommentPrefix + ([string]$intCommentCount)) + '". Key''s value will be changed to: "' + $Matches[1] + '"')
                                 ($hashtableIni.Item($strEffectiveSection)).Item($strCommentPrefix + ([string]$intCommentCount)) = $Matches[1]
                             } else {
                                 ($hashtableIni.Item($strEffectiveSection)).Add($strCommentPrefix + ([string]$intCommentCount), $Matches[1])
@@ -218,7 +238,7 @@ function Convert-IniToHashTable {
 
                 if ($null -ne $strKey) {
                     if (($hashtableIni.Item($strEffectiveSection)).ContainsKey($strKey)) {
-                        Write-Warning ("File `"" + $strFilePath + "`", section `"" + $strEffectiveSection + "`" already unexpectedly contains a key `"" + $strKey + "`" with value `"" + ($hashtableIni.Item($strEffectiveSection)).Item($strKey) + "`". Key's value will be changed to: null")
+                        Write-Warning ('File "' + $strFilePath + '", section "' + $strEffectiveSection + '" already unexpectedly contains a key "' + $strKey + '" with value "' + ($hashtableIni.Item($strEffectiveSection)).Item($strKey) + '". Key''s value will be changed to: null')
                         ($hashtableIni.Item($strEffectiveSection)).Item($strKey) = $strValue
                     } else {
                         ($hashtableIni.Item($strEffectiveSection)).Add($strKey, $strValue)
