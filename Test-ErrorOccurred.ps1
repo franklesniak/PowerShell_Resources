7332b5ef266214fb849dba3f28a344690f8d8972
Frank Lesniak <franklesniak@hotmail.com>
2021-01-05 13:36:54 -0600

    Add error support functions and Get-ObjectType

---
+++ Test-ErrorOccurred.ps1
@@ -0,0 +1,64 @@
+function Test-ErrorOccurred {
+    #region FunctionHeader ####################################################
+    # Function accepts two positional arguments:
+    #
+    # The first argument is a reference (memory pointer) to the last error that had occurred
+    #   prior to calling the command in question - that is, the command that we want to test
+    #   to see if an error occurred.
+    #
+    # The second argument is a reference to the last error that had occurred as-of the
+    #   completion of the command in question
+    #
+    # Function returns $true if it appears that an error occurred; $false otherwise
+    #
+    # Version: 1.0.20210105.0
+    #endregion FunctionHeader ####################################################
+
+    #region License ####################################################
+    # Copyright (c) 2021 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of this
+    # software and associated documentation files (the "Software"), to deal in the Software
+    # without restriction, including without limitation the rights to use, copy, modify, merge,
+    # publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+    # persons to whom the Software is furnished to do so, subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in all copies or
+    # substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+    # PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+    # FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+    # DEALINGS IN THE SOFTWARE.
+    #endregion License ####################################################
+
+    #region DownloadLocationNotice ####################################################
+    # The most up-to-date version of this script can be found on the author's GitHub repository
+    # at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice ####################################################
+
+    # TO-DO: Validate input
+
+    $boolErrorOccurred = $false
+    if (($null -ne ($args[0])) -and ($null -ne ($args[1]))) {
+        # Both not $null
+        if ((($args[0]).Value) -ne (($args[1]).Value)) {
+            $boolErrorOccurred = $true
+        }
+    } else {
+        # One is $null, or both are $null
+        # NOTE: ($args[0]) could be non-null, while ($args[1])
+        # could be null if $error was cleared; this does not indicate an error.
+        # So:
+        # If both are null, no error
+        # If ($args[0]) is null and ($args[1]) is non-null, error
+        # If ($args[0]) is non-null and ($args[1]) is null, no error
+        if (($null -eq ($args[0])) -and ($null -ne ($args[1]))) {
+            $boolErrorOccurred
+        }
+    }
+
+    $boolErrorOccurred
+}
