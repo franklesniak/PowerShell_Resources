4d7b08925e7adba3b196f3c8b019f712146d2fa3
Frank Lesniak <franklesniak@hotmail.com>
2023-06-13 13:16:41 -0500

    Clean up Get-PSVersion

--- Get-PSVersion.ps1
+++ Get-PSVersion.ps1
@@ -1,54 +1,58 @@
-# Get-PSVersion
-$strThisScriptLoadedFlagVariableName = "__FRANKLESNIAK__GETPSVERSION__LOADED"
-$strThisScriptVersionNumberVariableName = "__FRANKLESNIAK__GETPSVERSION__NUMBER"
-$strThisScriptVersionNumber = [version]"1.0.0.20200730"
-
-#region License
-###############################################################################################
-# Copyright 2020 Frank Lesniak
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-# and associated documentation files (the "Software"), to deal in the Software without
-# restriction, including without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or
-# substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-###############################################################################################
-#endregion License
-
-#region PreventReruns
-if (Test-Path ("variable:\" + $strThisScriptLoadedFlagVariableName)) {
-    if (Test-Path ("variable:\" + $strThisScriptVersionNumberVariableName)) {
-        if ((Get-Variable -Name $strThisScriptVersionNumberVariableName).Value -ge $strThisScriptVersionNumber) {
-            break
-        } else {
-            (Get-Variable -Name $strThisScriptVersionNumberVariableName).Value = $strThisScriptVersionNumber
-        }
-    } else {
-        New-Variable -Name $strThisScriptVersionNumberVariableName -Value $strThisScriptVersionNumber
-    }
-} else {
-    New-Variable -Name $strThisScriptLoadedFlagVariableName -Value $true
-    if (Test-Path ("variable:\" + $strThisScriptVersionNumberVariableName)) {
-        (Get-Variable -Name $strThisScriptVersionNumberVariableName).Value = $strThisScriptVersionNumber
-    } else {
-        New-Variable -Name $strThisScriptVersionNumberVariableName -Value $strThisScriptVersionNumber
-    }
-}
-#endregion PreventReruns
-
 function Get-PSVersion {
+    <#
+    .SYNOPSIS
+    Returns the version of PowerShell that is running
+
+    .DESCRIPTION
+    Returns the version of PowerShell that is running, including on the original
+    release of Windows PowerShell (version 1.0)
+
+    .EXAMPLE
+    Get-PSVersion
+
+    This example returns the version of PowerShell that is running. On versions of
+    PowerShell greater than or equal to version 2.0, this function returns the
+    equivalent of $PSVersionTable.PSVersion
+
+    .OUTPUTS
+    A [version] object representing the version of PowerShell that is running
+
+    .NOTES
+    PowerShell 1.0 does not have a $PSVersionTable variable, so this function returns
+    [version]('1.0') on PowerShell 1.0
+    #>
+
+    [CmdletBinding()]
+    [OutputType([version])]
+
+    param ()
+
+    #region License ################################################################
+    # Copyright (c) 2023 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of
+    # this software and associated documentation files (the "Software"), to deal in the
+    # Software without restriction, including without limitation the rights to use,
+    # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    # Software, and to permit persons to whom the Software is furnished to do so,
+    # subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License ################################################################
+
+    $versionThisFunction = [version]('1.0.20230613.0')
+
     if (Test-Path variable:\PSVersionTable) {
         $PSVersionTable.PSVersion
     } else {
-        [version]("1.0")
+        [version]('1.0')
     }
 }
