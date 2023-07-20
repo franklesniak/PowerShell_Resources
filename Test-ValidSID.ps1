1c8747df2682a2f0dfa357500fb88703161cc06a
Frank Lesniak <franklesniak@hotmail.com>
2023-07-19 22:19:58 -0500

    Test for SID without throwing background errors

--- Test-ValidSID.ps1
+++ Test-ValidSID.ps1
@@ -1,34 +1,33 @@
 function Test-ValidSID {
-    <#
-    .SYNOPSIS
-    Tests whether the supplied parameter is a valid security identifier (SID)
-
-    .DESCRIPTION
-    The function tests whether the supplied arugment is a security identifier (SID).
-    If the supplied argument is a SID, the function returns $true. If the supplied
-    argument is not a SID, the function returns $false.
-
-    .EXAMPLE
-    Test-ValidSID -ObjectToTest 'S-1-5-21-1234567890-1234567890-1234567890-1000'
-
-    .NOTES
-    The function was written by Frank Lesniak, licensed under the MIT license, and is
-    available on GitHub at https://github.com/franklesniak/PowerShell_Resources
-    #>
+    #region FunctionHeader #########################################################
+    # This function tests whether the supplied arugment is a security identifier (SID).
+    # If the supplied argument is a SID, the function returns $true. If the supplied
+    # argument is not a SID, the function returns $false.
+    #
+    # One positional argument is required: an object to be evaluated to determine
+    # whether it is a SID
+    #
+    # The function returns $true if the supplied object is a SID; $false otherwise
+    #
+    # Example usage:
+    # $boolResult = Test-ValidSID 'S-1-5-21-1234567890-1234567890-1234567890-1000'
+    #
+    # Version: 2.0.20230719.0
+    #endregion FunctionHeader #########################################################
 
     #region License ################################################################
-    # Copyright 2022 Frank Lesniak
-
+    # Copyright (c) 2023 Frank Lesniak
+    #
     # Permission is hereby granted, free of charge, to any person obtaining a copy of
     # this software and associated documentation files (the "Software"), to deal in the
     # Software without restriction, including without limitation the rights to use,
     # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
     # Software, and to permit persons to whom the Software is furnished to do so,
     # subject to the following conditions:
-
+    #
     # The above copyright notice and this permission notice shall be included in all
     # copies or substantial portions of the Software.
-
+    #
     # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
     # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -37,16 +36,49 @@ function Test-ValidSID {
     # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     #endregion License ################################################################
 
-    [cmdletbinding()]
-    [OutputType([Boolean])]
-    param (
-        [Parameter(Mandatory = $true)]$ObjectToTest
-    )
+    #region Acknowledgements #######################################################
+    # Thanks to Friedrich Weinmann who suggested an alternative way to test for a SID:
+    # https://twitter.com/FredWeinmann/status/1675513443615404032?s=20
+    # retrieved on 2023-07-19
+    #endregion Acknowledgements #######################################################
 
     #region FunctionsToSupportErrorHandling ########################################
     function Get-ReferenceToLastError {
+        #region FunctionHeader #####################################################
         # Function returns $null if no errors on on the $error stack;
-        # Otherwise, function returns a reference (memory pointer) to the last error that occurred.
+        # Otherwise, function returns a reference (memory pointer) to the last error
+        # that occurred.
+        #
+        # Version: 1.0.20230709.0
+        #endregion FunctionHeader #####################################################
+
+        #region License ############################################################
+        # Copyright (c) 2023 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining a copy
+        # of this software and associated documentation files (the "Software"), to deal
+        # in the Software without restriction, including without limitation the rights
+        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        # copies of the Software, and to permit persons to whom the Software is
+        # furnished to do so, subject to the following conditions:
+        #
+        # The above copyright notice and this permission notice shall be included in
+        # all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ############################################################
+
+        #region DownloadLocationNotice #############################################
+        # The most up-to-date version of this script can be found on the author's
+        # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #############################################
+
         if ($error.Count -gt 0) {
             [ref]($error[0])
         } else {
@@ -55,16 +87,47 @@ function Test-ValidSID {
     }
 
     function Test-ErrorOccurred {
+        #region FunctionHeader #####################################################
         # Function accepts two positional arguments:
         #
-        # The first argument is a reference (memory pointer) to the last error that had occurred
-        #   prior to calling the command in question - that is, the command that we want to test
-        #   to see if an error occurred.
+        # The first argument is a reference (memory pointer) to the last error that had
+        # occurred prior to calling the command in question - that is, the command that
+        # we want to test to see if an error occurred.
         #
-        # The second argument is a reference to the last error that had occurred as-of the
-        #   completion of the command in question
+        # The second argument is a reference to the last error that had occurred as-of
+        # the completion of the command in question
         #
         # Function returns $true if it appears that an error occurred; $false otherwise
+        #
+        # Version: 1.0.20230709.0
+        #endregion FunctionHeader #####################################################
+
+        #region License ############################################################
+        # Copyright (c) 2023 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining a copy
+        # of this software and associated documentation files (the "Software"), to deal
+        # in the Software without restriction, including without limitation the rights
+        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        # copies of the Software, and to permit persons to whom the Software is
+        # furnished to do so, subject to the following conditions:
+        #
+        # The above copyright notice and this permission notice shall be included in
+        # all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ############################################################
+
+        #region DownloadLocationNotice #################################################
+        # The most up-to-date version of this script can be found on the author's GitHub
+        # repository at https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #################################################
 
         # TO-DO: Validate input
 
@@ -92,8 +155,11 @@ function Test-ValidSID {
     #endregion FunctionsToSupportErrorHandling ########################################
 
     trap {
-        # Intentionally left empty to prevent terminating errors from halting processing
+        # Intentionally left empty to prevent terminating errors from halting
+        # processing
     }
+
+    $objSID = $null
 
     # Retrieve the newest error on the stack prior to running the command to determine
     # if the object is a SID
@@ -109,7 +175,10 @@ function Test-ValidSID {
     $global:ErrorActionPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
     # Run the command to determine if the object is a SID
-    $objSID = New-Object -TypeName System.Security.Principal.SecurityIdentifier -ArgumentList $ObjectToTest
+    $objSID = ($args[0]) -as [System.Security.Principal.SecurityIdentifier]
+
+    # Restore the former error preference
+    $global:ErrorActionPreference = $actionPreferenceFormerErrorPreference
 
     # Retrieve the newest error on the error stack
     $refNewestCurrentError = Get-ReferenceToLastError
@@ -117,6 +186,6 @@ function Test-ValidSID {
     if (Test-ErrorOccurred $refLastKnownError $refNewestCurrentError) {
         return $false
     } else {
-        return $true
+        return ($null -ne $objSID)
     }
 }
