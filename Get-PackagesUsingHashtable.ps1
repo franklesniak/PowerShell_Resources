ecdadc9262e2c51d6cbe3d156f3159f722465e7c
Frank Lesniak <franklesniak@hotmail.com>
2024-03-12 16:57:35 -0500

    Look for installed NuGet packages

---
+++ Get-PackagesUsingHashtable.ps1
@@ -0,0 +1,95 @@
+function Get-PackagesUsingHashtable {
+    <#
+    .SYNOPSIS
+    Gets a list of installed "software packages" (typically NuGet packages) for each
+    entry in a hashtable.
+
+    .DESCRIPTION
+    The Get-PackagesUsingHashtable function steps through each entry in the supplied
+    hashtable. If a corresponding package is installed, then the information about
+    the newest version of that package is stored in the value of the hashtable entry
+    corresponding to the software package.
+
+    .PARAMETER $ReferenceToHashtable
+    Is a reference to a hashtable. The value of the reference should be a hashtable
+    with keys that are the names software packages and values that are initialized
+    to be $null.
+
+    .EXAMPLE
+    $hashtablePackageNameToInstalledPackages = @{}
+    $hashtablePackageNameToInstalledPackages.Add('Accord', $null)
+    $hashtablePackageNameToInstalledPackages.Add('Accord.Math', $null)
+    $hashtablePackageNameToInstalledPackages.Add('Accord.Statistics', $null)
+    $hashtablePackageNameToInstalledPackages.Add('Accord.MachineLearning', $null)
+    $refHashtablePackageNameToInstalledPackages = [ref]$hashtablePackageNameToInstalledPackages
+    Get-PackagesUsingHashtable -ReferenceToHashtable $refHashtablePackageNameToInstalledPackages
+
+    This example checks each of the four software packages specified. For each software
+    package specified, if the software package is installed, the value of the hashtable
+    entry will be set to the newest-installed version of the package. If the software
+    package is not installed, the value of the hashtable entry remains $null.
+
+    .OUTPUTS
+    None
+
+    .NOTES
+    Requires PowerShell v5.0 or newer
+    #>
+
+    #region License
+    # Copyright 2024 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of this
+    # software and associated documentation files (the “Software”), to deal in the
+    # Software without restriction, including without limitation the rights to use, copy,
+    # modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+    # and to permit persons to whom the Software is furnished to do so, subject to the
+    # following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+    # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+    # PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+    # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+    # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License
+
+    #region DownloadLocationNotice
+    # The most up-to-date version of this script can be found on the author's GitHub repository
+    # at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice
+
+    # Version 1.0.20240312.0
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)][ref]$ReferenceToHashtable
+    )
+
+    $WarningPreferenceAtStartOfFunction = $WarningPreference
+    $VerbosePreferenceAtStartOfFunction = $VerbosePreference
+    $DebugPreferenceAtStartOfFunction = $DebugPreference
+
+    $arrPackagesToGet = @(($ReferenceToHashtable.Value).Keys)
+
+    $WarningPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+    $VerbosePreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+    $DebugPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+    $arrPackagesInstalled = @(Get-Package)
+    $WarningPreference = $WarningPreferenceAtStartOfFunction
+    $VerbosePreference = $VerbosePreferenceAtStartOfFunction
+    $DebugPreference = $DebugPreferenceAtStartOfFunction
+
+    for ($intCounter = 0; $intCounter -lt $arrPackagesToGet.Count; $intCounter++) {
+        Write-Debug ('Checking for ' + $arrPackagesToGet[$intCounter] + ' software package...')
+        $arrMatchingPackages = @($arrPackagesInstalled | Where-Object { $_.Name -eq $arrPackagesToGet[$intCounter] })
+        if ($arrMatchingPackages.Count -eq 0) {
+            ($ReferenceToHashtable.Value).Item($arrPackagesToGet[$intCounter]) = $null
+        } else {
+            ($ReferenceToHashtable.Value).Item($arrPackagesToGet[$intCounter]) = $arrMatchingPackages[0]
+        }
+    }
+}
