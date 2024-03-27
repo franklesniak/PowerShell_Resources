1d80fd61ca49bf8191878043896143effa8e8ae6
Frank Lesniak <franklesniak@hotmail.com>
2024-03-26 20:49:26 -0500

    Fix formatting for nested function action

--- Get-PSVersion.ps1
+++ Get-PSVersion.ps1
@@ -5,47 +5,48 @@ function Get-PSVersion {
     # Example:
     # Get-PSVersion
     #
-    # This example returns the version of PowerShell that is running. On versions of
-    # PowerShell greater than or equal to version 2.0, this function returns the
+    # This example returns the version of PowerShell that is running. On versions
+    # of PowerShell greater than or equal to version 2.0, this function returns the
     # equivalent of $PSVersionTable.PSVersion
     #
-    # The function outputs a [version] object representing the version of PowerShell
-    # that is running
+    # The function outputs a [version] object representing the version of
+    # PowerShell that is running
     #
-    # PowerShell 1.0 does not have a $PSVersionTable variable, so this function returns
-    # [version]('1.0') on PowerShell 1.0
+    # PowerShell 1.0 does not have a $PSVersionTable variable, so this function
+    # returns [version]('1.0') on PowerShell 1.0
 
-    #region License ################################################################
-    # Copyright (c) 2023 Frank Lesniak
+    #region License ############################################################
+    # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy of
-    # this software and associated documentation files (the "Software"), to deal in the
-    # Software without restriction, including without limitation the rights to use,
-    # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
-    # Software, and to permit persons to whom the Software is furnished to do so,
-    # subject to the following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a copy
+    # of this software and associated documentation files (the "Software"), to deal
+    # in the Software without restriction, including without limitation the rights
+    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    # copies of the Software, and to permit persons to whom the Software is
+    # furnished to do so, subject to the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included in all
-    # copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be included in
+    # all copies or substantial portions of the Software.
     #
     # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
-    # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-    # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ################################################################
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ############################################################
 
-    #region DownloadLocationNotice #################################################
-    # The most up-to-date version of this script can be found on the author's GitHub
-    # repository at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #################################################
+    #region DownloadLocationNotice #############################################
+    # The most up-to-date version of this script can be found on the author's
+    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice #############################################
 
-    $versionThisFunction = [version]('1.0.20230709.0')
+    $versionThisFunction = [version]('1.0.20240326.0')
 
     if (Test-Path variable:\PSVersionTable) {
-        $PSVersionTable.PSVersion
+        return ($PSVersionTable.PSVersion)
     } else {
-        [version]('1.0')
+        return ([version]('1.0'))
     }
 }
