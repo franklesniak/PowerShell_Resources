33c9b6d323675e3260ce8319944c89b6c64f57cf
Frank Lesniak <franklesniak@hotmail.com>
2024-04-02 20:41:57 -0500

    Fix formatting

--- Test-PowerShellModuleInstalledUsingHashtable.ps1
+++ Test-PowerShellModuleInstalledUsingHashtable.ps1
@@ -29,37 +29,37 @@ function Get-PowerShellModuleUsingHashtable {
     None
     #>
 
-    #region License
-    # Copyright 2024 Frank Lesniak
+    #region License ################################################################
+    # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy of this
-    # software and associated documentation files (the “Software”), to deal in the
-    # Software without restriction, including without limitation the rights to use, copy,
-    # modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-    # and to permit persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of
+    # this software and associated documentation files (the "Software"), to deal in the
+    # Software without restriction, including without limitation the rights to use,
+    # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    # Software, and to permit persons to whom the Software is furnished to do so,
+    # subject to the following conditions:
     #
     # The above copyright notice and this permission notice shall be included in all
     # copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-    # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-    # PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-    # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-    # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License ################################################################
 
-    #region DownloadLocationNotice
-    # The most up-to-date version of this script can be found on the author's GitHub repository
-    # at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice
+    #region DownloadLocationNotice  ################################################
+    # The most up-to-date version of this script can be found on the author's GitHub
+    # repository at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice  ################################################
 
-    # Version 1.0.20240326.0
+    # Version 1.0.20240401.0
 
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $true)] [ref] $ReferenceToHashtable
+        [Parameter(Mandatory = $true)][ref]$ReferenceToHashtable
     )
 
     $VerbosePreferenceAtStartOfFunction = $VerbosePreference
@@ -153,33 +153,33 @@ function Test-PowerShellModuleInstalledUsingHashtable {
     [boolean] - Returns $true if all modules are installed; otherwise, returns $false.
     #>
 
-    #region License
-    # Copyright 2024 Frank Lesniak
+    #region License ################################################################
+    # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy of this
-    # software and associated documentation files (the “Software”), to deal in the
-    # Software without restriction, including without limitation the rights to use, copy,
-    # modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
-    # and to permit persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of
+    # this software and associated documentation files (the "Software"), to deal in the
+    # Software without restriction, including without limitation the rights to use,
+    # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    # Software, and to permit persons to whom the Software is furnished to do so,
+    # subject to the following conditions:
     #
     # The above copyright notice and this permission notice shall be included in all
     # copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-    # INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
-    # PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-    # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-    # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
-    # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License ################################################################
 
-    #region DownloadLocationNotice
-    # The most up-to-date version of this script can be found on the author's GitHub repository
-    # at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice
+    #region DownloadLocationNotice  ################################################
+    # The most up-to-date version of this script can be found on the author's GitHub
+    # repository at https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice  ################################################
 
-    # Version 1.1.20240326.0
+    # Version 1.1.20240401.0
 
     [CmdletBinding()]
     [OutputType([Boolean])]
