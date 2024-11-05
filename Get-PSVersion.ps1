32ec1e8a089bbd4d3c3a1d0f93467b14f1c5786f
Frank Lesniak <franklesniak@hotmail.com>
2024-11-05 10:14:52 -0600

    Cleanup for function nesting

--- Get-PSVersion.ps1
+++ Get-PSVersion.ps1
@@ -1,13 +1,14 @@
 function Get-PSVersion {
-    # Returns the version of PowerShell that is running, including on the original
-    # release of Windows PowerShell (version 1.0)
+    #region FunctionHeader #################################################
+    # Returns the version of PowerShell that is running, including on the
+    # original release of Windows PowerShell (version 1.0)
     #
     # Example:
     # Get-PSVersion
     #
-    # This example returns the version of PowerShell that is running. On versions
-    # of PowerShell greater than or equal to version 2.0, this function returns the
-    # equivalent of $PSVersionTable.PSVersion
+    # This example returns the version of PowerShell that is running. On
+    # versions of PowerShell greater than or equal to version 2.0, this
+    # function returns the equivalent of $PSVersionTable.PSVersion
     #
     # The function outputs a [version] object representing the version of
     # PowerShell that is running
@@ -15,34 +16,37 @@ function Get-PSVersion {
     # PowerShell 1.0 does not have a $PSVersionTable variable, so this function
     # returns [version]('1.0') on PowerShell 1.0
     #
-    # Version 1.0.20240917.0
+    # Version 1.0.20241105.0
+    #endregion FunctionHeader #################################################
 
-    #region License ############################################################
+    #region License ########################################################
     # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy
-    # of this software and associated documentation files (the "Software"), to deal
-    # in the Software without restriction, including without limitation the rights
-    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    # copies of the Software, and to permit persons to whom the Software is
-    # furnished to do so, subject to the following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a
+    # copy of this software and associated documentation files (the
+    # "Software"), to deal in the Software without restriction, including
+    # without limitation the rights to use, copy, modify, merge, publish,
+    # distribute, sublicense, and/or sell copies of the Software, and to permit
+    # persons to whom the Software is furnished to do so, subject to the
+    # following conditions:
     #
-    # The above copyright notice and this permission notice shall be included in
-    # all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be included
+    # in all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    # SOFTWARE.
-    #endregion License ############################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+    # USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License ########################################################
 
-    #region DownloadLocationNotice #############################################
+    #region DownloadLocationNotice #########################################
     # The most up-to-date version of this script can be found on the author's
-    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #############################################
+    # GitHub repository at:
+    # https://github.com/franklesniak/PowerShell_Resources
+    #endregion DownloadLocationNotice #########################################
 
     if (Test-Path variable:\PSVersionTable) {
         return ($PSVersionTable.PSVersion)
