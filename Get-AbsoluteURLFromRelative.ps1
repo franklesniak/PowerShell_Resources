2284f67b914d2600b7c018be7e93b1e6a1cef63c
Frank Lesniak <franklesniak@hotmail.com>
2024-11-05 11:35:17 -0600

    Code cleanup

--- Get-AbsoluteURLFromRelative.ps1
+++ Get-AbsoluteURLFromRelative.ps1
@@ -1,46 +1,17 @@
-# Get-AbsoluteURLFromRelative.ps1 contains one function (Get-AbsoluteURLFromRelative) that is
-# designed to take a (potentially) relative URL and build its absolute (full) URL, including
-# its parent path.
-
-$strThisScriptVersionNumber = [version]'1.0.20201004.0'
-
-#region License
-###############################################################################################
-# Copyright 2020 Frank Lesniak
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-# and associated documentation files (the "Software"), to deal in the Software without
-# restriction, including without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
-# Software is furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all copies or
-# substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-###############################################################################################
-#endregion License
-
-#region DownloadLocationNotice
-# The most up-to-date version of this script can be found on the author's GitHub repository
-# at https://github.com/franklesniak/PowerShell_Resources
-#endregion DownloadLocationNotice
-
 function Get-AbsoluteURLFromRelative {
-    # This functions takes a potentially relative URL (/etc/foo.html) and turns it into an
-    # absolute URL if it is, in fact, a relative URL. If the URL is an absolute URL, then the
-    # function simply returns the absolute URL.
+    #region FunctionHeader #####################################################
+    # This functions takes a potentially relative URL (/etc/foo.html) and turns it
+    # into an absolute URL if it is, in fact, a relative URL. If the URL is an
+    # absolute URL, then the function simply returns the absolute URL.
     #
     # The function takes two positional arguments.
     #
-    # The first argument is a string containing the base URL (i.e., the parent URL) from which
-    # the second URL is derived
+    # The first argument is a string containing the base URL (i.e., the parent URL)
+    # from which the second URL is derived
     #
     # The second argument is a string containing the (potentially) relative URL.
+    #
+    # The function returns a string that represents the absolute URL.
     #
     # Example 1:
     # $strURLBase = 'http://foo.net/stuff/index.html'
@@ -60,13 +31,38 @@ function Get-AbsoluteURLFromRelative {
     # $strURLAbsolute = Get-AbsoluteURLFromRelative $strURLBase $strURLRelative
     # # $strURLAbsolute is 'http://foo.net/stuff/downloads/list.txt'
     #
-    # Note: this function is converted from https://stackoverflow.com/a/34603567/2134110
+    # Version 1.0.20241105.0
+    #endregion FunctionHeader #####################################################
+
+    #region License ################################################################
+    # Copyright (c) 2024 Frank Lesniak
+    #
+    # Permission is hereby granted, free of charge, to any person obtaining a copy of
+    # this software and associated documentation files (the "Software"), to deal in the
+    # Software without restriction, including without limitation the rights to use,
+    # copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+    # Software, and to permit persons to whom the Software is furnished to do so,
+    # subject to the following conditions:
+    #
+    # The above copyright notice and this permission notice shall be included in all
+    # copies or substantial portions of the Software.
+    #
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    # FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    # COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+    # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+    # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License ################################################################
+
+    #region Acknowledgements #######################################################
+    # Note: this function is converted from
+    # https://stackoverflow.com/a/34603567/2134110
     # Thanks to Vikash Rathee for pointing me in the right direction
+    #endregion Acknowledgements #######################################################
 
     $strURLBase = $args[0]
     $strURLRelative = $args[1]
-
-    $strThisFunctionVersionNumber = [version]'1.0.20201004.0'
 
     $uriKindRelativeOrAbsolute = [System.UriKind]::RelativeOrAbsolute
     $uriWorking = New-Object -TypeName 'System.Uri' -ArgumentList @($strURLRelative, $uriKindRelativeOrAbsolute)
@@ -74,5 +70,5 @@ function Get-AbsoluteURLFromRelative {
         $uriBase = New-Object -TypeName 'System.Uri' -ArgumentList @($strURLBase)
         $uriWorking = New-Object -TypeName 'System.Uri' -ArgumentList @($uriBase, $strURLRelative)
     }
-    $uriWorking.ToString()
+    return ($uriWorking.ToString())
 }
