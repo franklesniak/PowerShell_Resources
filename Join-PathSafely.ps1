a9b50aabaab34e969f07275a004bd65275d65200
Frank Lesniak <franklesniak@hotmail.com>
2024-11-07 13:04:16 -0600

    Bring in updated functions

--- Join-PathSafely.ps1
+++ Join-PathSafely.ps1
@@ -8,88 +8,118 @@ function Join-PathSafely {
     # TODO: Write a proper function header
 
     function Get-ReferenceToLastError {
-        #region FunctionHeader #####################################################
+        #region FunctionHeader #################################################
         # Function returns $null if no errors on on the $error stack;
-        # Otherwise, function returns a reference (memory pointer) to the last error
-        # that occurred.
+        # Otherwise, function returns a reference (memory pointer) to the last
+        # error that occurred.
         #
-        # Version: 1.0.20240127.0
-        #endregion FunctionHeader #####################################################
+        # Version: 1.0.20241105.0
+        #endregion FunctionHeader #################################################
 
-        #region License ############################################################
+        #region License ########################################################
         # Copyright (c) 2024 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining a copy
-        # of this software and associated documentation files (the "Software"), to deal
-        # in the Software without restriction, including without limitation the rights
-        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        # copies of the Software, and to permit persons to whom the Software is
-        # furnished to do so, subject to the following conditions:
+        # Permission is hereby granted, free of charge, to any person obtaining a
+        # copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to permit
+        # persons to whom the Software is furnished to do so, subject to the
+        # following conditions:
         #
-        # The above copyright notice and this permission notice shall be included in
-        # all copies or substantial portions of the Software.
+        # The above copyright notice and this permission notice shall be included
+        # in all copies or substantial portions of the Software.
         #
-        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        # SOFTWARE.
-        #endregion License ############################################################
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+        # USE OR OTHER DEALINGS IN THE SOFTWARE.
+        #endregion License ########################################################
 
-        #region DownloadLocationNotice #############################################
+        #region DownloadLocationNotice #########################################
         # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #############################################
+        # GitHub repository at:
+        # https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #########################################
 
-        if ($error.Count -gt 0) {
-            [ref]($error[0])
+        if ($Error.Count -gt 0) {
+            return ([ref]($Error[0]))
         } else {
-            $null
+            return $null
         }
     }
-    function Get-ReferenceToLastError {
-        #region FunctionHeader #####################################################
-        # Function returns $null if no errors on on the $error stack;
-        # Otherwise, function returns a reference (memory pointer) to the last error
-        # that occurred.
-        #
-        # Version: 1.0.20240127.0
-        #endregion FunctionHeader #####################################################
 
-        #region License ############################################################
+    function Test-ErrorOccurred {
+        #region FunctionHeader #################################################
+        # Function accepts two positional arguments:
+        #
+        # The first argument is a reference (memory pointer) to the last error that
+        # had occurred prior to calling the command in question - that is, the
+        # command that we want to test to see if an error occurred.
+        #
+        # The second argument is a reference to the last error that had occurred
+        # as-of the completion of the command in question.
+        #
+        # Function returns $true if it appears that an error occurred; $false
+        # otherwise
+        #
+        # Version: 1.0.20241105.0
+        #endregion FunctionHeader #################################################
+
+        #region License ########################################################
         # Copyright (c) 2024 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining a copy
-        # of this software and associated documentation files (the "Software"), to deal
-        # in the Software without restriction, including without limitation the rights
-        # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        # copies of the Software, and to permit persons to whom the Software is
-        # furnished to do so, subject to the following conditions:
+        # Permission is hereby granted, free of charge, to any person obtaining a
+        # copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to permit
+        # persons to whom the Software is furnished to do so, subject to the
+        # following conditions:
         #
-        # The above copyright notice and this permission notice shall be included in
-        # all copies or substantial portions of the Software.
+        # The above copyright notice and this permission notice shall be included
+        # in all copies or substantial portions of the Software.
         #
-        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        # SOFTWARE.
-        #endregion License ############################################################
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+        # USE OR OTHER DEALINGS IN THE SOFTWARE.
+        #endregion License ########################################################
 
-        #region DownloadLocationNotice #############################################
+        #region DownloadLocationNotice #########################################
         # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #############################################
+        # GitHub repository at:
+        # https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #########################################
 
-        if ($error.Count -gt 0) {
-            [ref]($error[0])
+        # TO-DO: Validate input
+
+        $boolErrorOccurred = $false
+        if (($null -ne ($args[0])) -and ($null -ne ($args[1]))) {
+            # Both not $null
+            if ((($args[0]).Value) -ne (($args[1]).Value)) {
+                $boolErrorOccurred = $true
+            }
         } else {
-            $null
+            # One is $null, or both are $null
+            # NOTE: ($args[0]) could be non-null, while ($args[1])
+            # could be null if $error was cleared; this does not indicate an error.
+            # So:
+            # If both are null, no error
+            # If ($args[0]) is null and ($args[1]) is non-null, error
+            # If ($args[0]) is non-null and ($args[1]) is null, no error
+            if (($null -eq ($args[0])) -and ($null -ne ($args[1]))) {
+                $boolErrorOccurred = $true
+            }
         }
+
+        return $boolErrorOccurred
     }
 
     trap {
