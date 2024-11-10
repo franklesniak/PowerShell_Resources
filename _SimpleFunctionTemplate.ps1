4d0fc6aa5d44b92e5fb73e6e5cfabee0cfd34772
Frank Lesniak <franklesniak@hotmail.com>
2024-11-10 16:57:47 -0700

    Nest error handling functions within templates

--- _SimpleFunctionTemplate.ps1
+++ _SimpleFunctionTemplate.ps1
@@ -1,5 +1,5 @@
 #region License ####################################################################
-# Copyright 2023 Frank Lesniak
+# Copyright 2024 Frank Lesniak
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this
 # software and associated documentation files (the "Software"), to deal in the Software
@@ -19,121 +19,7 @@
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #endregion License ####################################################################
 
-# Function template version: 1.1.20240127.0
-
-################### THE FOLLOWING FUNCTIONS MUST BE INCLUDED FOR ERROR HANDLING TO WORK  ###################
-################### YOU MAY ALSO EMBED THEM INTO THE FUNCTION YOU ARE CREATING  ###################
-#region FunctionsToSupportErrorHandling ############################################
-function Get-ReferenceToLastError {
-    #region FunctionHeader #####################################################
-    # Function returns $null if no errors on on the $error stack;
-    # Otherwise, function returns a reference (memory pointer) to the last error
-    # that occurred.
-    #
-    # Version: 1.0.20240127.0
-    #endregion FunctionHeader #####################################################
-
-    #region License ############################################################
-    # Copyright (c) 2024 Frank Lesniak
-    #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy
-    # of this software and associated documentation files (the "Software"), to deal
-    # in the Software without restriction, including without limitation the rights
-    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    # copies of the Software, and to permit persons to whom the Software is
-    # furnished to do so, subject to the following conditions:
-    #
-    # The above copyright notice and this permission notice shall be included in
-    # all copies or substantial portions of the Software.
-    #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    # SOFTWARE.
-    #endregion License ############################################################
-
-    #region DownloadLocationNotice #############################################
-    # The most up-to-date version of this script can be found on the author's
-    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #############################################
-
-    if ($error.Count -gt 0) {
-        [ref]($error[0])
-    } else {
-        $null
-    }
-}
-
-function Test-ErrorOccurred {
-    #region FunctionHeader #####################################################
-    # Function accepts two positional arguments:
-    #
-    # The first argument is a reference (memory pointer) to the last error that had
-    # occurred prior to calling the command in question - that is, the command that
-    # we want to test to see if an error occurred.
-    #
-    # The second argument is a reference to the last error that had occurred as-of
-    # the completion of the command in question.
-    #
-    # Function returns $true if it appears that an error occurred; $false otherwise
-    #
-    # Version: 1.0.20240127.0
-    #endregion FunctionHeader #####################################################
-
-    #region License ############################################################
-    # Copyright (c) 2024 Frank Lesniak
-    #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy
-    # of this software and associated documentation files (the "Software"), to deal
-    # in the Software without restriction, including without limitation the rights
-    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    # copies of the Software, and to permit persons to whom the Software is
-    # furnished to do so, subject to the following conditions:
-    #
-    # The above copyright notice and this permission notice shall be included in
-    # all copies or substantial portions of the Software.
-    #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    # SOFTWARE.
-    #endregion License ############################################################
-
-    #region DownloadLocationNotice #############################################
-    # The most up-to-date version of this script can be found on the author's
-    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #############################################
-
-    # TO-DO: Validate input
-
-    $boolErrorOccurred = $false
-    if (($null -ne ($args[0])) -and ($null -ne ($args[1]))) {
-        # Both not $null
-        if ((($args[0]).Value) -ne (($args[1]).Value)) {
-            $boolErrorOccurred = $true
-        }
-    } else {
-        # One is $null, or both are $null
-        # NOTE: ($args[0]) could be non-null, while ($args[1])
-        # could be null if $error was cleared; this does not indicate an error.
-        # So:
-        # If both are null, no error
-        # If ($args[0]) is null and ($args[1]) is non-null, error
-        # If ($args[0]) is non-null and ($args[1]) is null, no error
-        if (($null -eq ($args[0])) -and ($null -ne ($args[1]))) {
-            $boolErrorOccurred
-        }
-    }
-
-    $boolErrorOccurred
-}
-#endregion FunctionsToSupportErrorHandling ############################################
+# Function template version: 1.2.20241110.0
 
 function Invoke-SimpleFunction {
     #region FunctionHeader #########################################################
@@ -231,6 +117,123 @@ function Invoke-SimpleFunction {
     # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
     # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
     #endregion Original Licenses ######################################################
+
+    #region FunctionsToSupportErrorHandling ########################################
+    function Get-ReferenceToLastError {
+        #region FunctionHeader #################################################
+        # Function returns $null if no errors on on the $error stack;
+        # Otherwise, function returns a reference (memory pointer) to the last
+        # error that occurred.
+        #
+        # Version: 1.0.20241105.0
+        #endregion FunctionHeader #################################################
+
+        #region License ########################################################
+        # Copyright (c) 2024 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining a
+        # copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to permit
+        # persons to whom the Software is furnished to do so, subject to the
+        # following conditions:
+        #
+        # The above copyright notice and this permission notice shall be included
+        # in all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+        # USE OR OTHER DEALINGS IN THE SOFTWARE.
+        #endregion License ########################################################
+
+        #region DownloadLocationNotice #########################################
+        # The most up-to-date version of this script can be found on the author's
+        # GitHub repository at:
+        # https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #########################################
+
+        if ($Error.Count -gt 0) {
+            return ([ref]($Error[0]))
+        } else {
+            return $null
+        }
+    }
+
+    function Test-ErrorOccurred {
+        #region FunctionHeader #################################################
+        # Function accepts two positional arguments:
+        #
+        # The first argument is a reference (memory pointer) to the last error that
+        # had occurred prior to calling the command in question - that is, the
+        # command that we want to test to see if an error occurred.
+        #
+        # The second argument is a reference to the last error that had occurred
+        # as-of the completion of the command in question.
+        #
+        # Function returns $true if it appears that an error occurred; $false
+        # otherwise
+        #
+        # Version: 1.0.20241105.0
+        #endregion FunctionHeader #################################################
+
+        #region License ########################################################
+        # Copyright (c) 2024 Frank Lesniak
+        #
+        # Permission is hereby granted, free of charge, to any person obtaining a
+        # copy of this software and associated documentation files (the
+        # "Software"), to deal in the Software without restriction, including
+        # without limitation the rights to use, copy, modify, merge, publish,
+        # distribute, sublicense, and/or sell copies of the Software, and to permit
+        # persons to whom the Software is furnished to do so, subject to the
+        # following conditions:
+        #
+        # The above copyright notice and this permission notice shall be included
+        # in all copies or substantial portions of the Software.
+        #
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+        # USE OR OTHER DEALINGS IN THE SOFTWARE.
+        #endregion License ########################################################
+
+        #region DownloadLocationNotice #########################################
+        # The most up-to-date version of this script can be found on the author's
+        # GitHub repository at:
+        # https://github.com/franklesniak/PowerShell_Resources
+        #endregion DownloadLocationNotice #########################################
+
+        # TO-DO: Validate input
+
+        $boolErrorOccurred = $false
+        if (($null -ne ($args[0])) -and ($null -ne ($args[1]))) {
+            # Both not $null
+            if ((($args[0]).Value) -ne (($args[1]).Value)) {
+                $boolErrorOccurred = $true
+            }
+        } else {
+            # One is $null, or both are $null
+            # NOTE: ($args[0]) could be non-null, while ($args[1])
+            # could be null if $error was cleared; this does not indicate an error.
+            # So:
+            # If both are null, no error
+            # If ($args[0]) is null and ($args[1]) is non-null, error
+            # If ($args[0]) is non-null and ($args[1]) is null, no error
+            if (($null -eq ($args[0])) -and ($null -ne ($args[1]))) {
+                $boolErrorOccurred = $true
+            }
+        }
+
+        return $boolErrorOccurred
+    }
+    #endregion FunctionsToSupportErrorHandling ########################################
 
     trap {
         # Intentionally left empty to prevent terminating errors from halting
