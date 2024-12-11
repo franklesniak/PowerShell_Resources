3b27aa6cb9b8cfcaf8b78df32cf1ea72564c2a58
Frank Lesniak <franklesniak@hotmail.com>
2024-12-11 11:22:36 -0600

    Shorten function width

--- Get-ReferenceToLastError.ps1
+++ Get-ReferenceToLastError.ps1
@@ -1,40 +1,41 @@
 function Get-ReferenceToLastError {
-    #region FunctionHeader #################################################
+    #region FunctionHeader #############################################
     # Function returns $null if no errors on on the $error stack;
     # Otherwise, function returns a reference (memory pointer) to the last
     # error that occurred.
     #
-    # Version: 1.0.20241105.0
-    #endregion FunctionHeader #################################################
+    # Version: 1.0.20241211.0
+    #endregion FunctionHeader #############################################
 
-    #region License ########################################################
+    #region License ####################################################
     # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a
-    # copy of this software and associated documentation files (the
+    # Permission is hereby granted, free of charge, to any person obtaining
+    # a copy of this software and associated documentation files (the
     # "Software"), to deal in the Software without restriction, including
     # without limitation the rights to use, copy, modify, merge, publish,
-    # distribute, sublicense, and/or sell copies of the Software, and to permit
-    # persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # distribute, sublicense, and/or sell copies of the Software, and to
+    # permit persons to whom the Software is furnished to do so, subject to
+    # the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included
-    # in all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be
+    # included in all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-    # USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ########################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+    # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+    # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ####################################################
 
-    #region DownloadLocationNotice #########################################
-    # The most up-to-date version of this script can be found on the author's
-    # GitHub repository at:
+    #region DownloadLocationNotice #####################################
+    # The most up-to-date version of this script can be found on the
+    # author's GitHub repository at:
     # https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #########################################
+    #endregion DownloadLocationNotice #####################################
 
     if ($Error.Count -gt 0) {
         return ([ref]($Error[0]))
