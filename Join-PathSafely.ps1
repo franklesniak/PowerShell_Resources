0d931f10e5850468240d92f255692a4284ab309b
Frank Lesniak <franklesniak@hotmail.com>
2024-12-11 11:21:48 -0600

    Shorten function widths

--- Join-PathSafely.ps1
+++ Join-PathSafely.ps1
@@ -1,5 +1,5 @@
 function Join-PathSafely {
-    #region FunctionHeader #####################################################
+    #region FunctionHeader #################################################
     # Combines two paths into a single path. This function is intended to be
     # used in situations where the Join-Path cmdlet may fail due to a variety
     # of reasons. This function is designed to suppress errors and return a
@@ -7,11 +7,11 @@ function Join-PathSafely {
     #
     # Three positional arguments are required:
     #
-    # The first argument is a reference to a string object that will be populated
-    # with the joined path (parent path + child path). If the operation was
-    # successful, the referenced string object will be populated with the joined
-    # path. If the operation was unsuccessful, the referenced string will be left
-    # unchanged.
+    # The first argument is a reference to a string object that will be
+    # populated with the joined path (parent path + child path). If the
+    # operation was successful, the referenced string object will be populated
+    # with the joined path. If the operation was unsuccessful, the referenced
+    # string will be left unchanged.
     #
     # The second argument is a string representing the parent part of the path.
     #
@@ -29,74 +29,76 @@ function Join-PathSafely {
     # $strJoinedPath = $null
     # $boolSuccess = Join-PathSafely ([ref]$strJoinedPath) $strParentPartOfPath $strChildPartOfPath
     #
-    # Version 1.0.20241112.1
-    #endregion FunctionHeader #####################################################
+    # Version 1.0.20241211.1
+    #endregion FunctionHeader #################################################
 
-    #region License ############################################################
+    #region License ########################################################
     # Copyright (c) 2024 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a copy
-    # of this software and associated documentation files (the "Software"), to deal
-    # in the Software without restriction, including without limitation the rights
-    # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    # copies of the Software, and to permit persons to whom the Software is
-    # furnished to do so, subject to the following conditions:
+    # Permission is hereby granted, free of charge, to any person obtaining a
+    # copy of this software and associated documentation files (the
+    # "Software"), to deal in the Software without restriction, including
+    # without limitation the rights to use, copy, modify, merge, publish,
+    # distribute, sublicense, and/or sell copies of the Software, and to permit
+    # persons to whom the Software is furnished to do so, subject to the
+    # following conditions:
     #
-    # The above copyright notice and this permission notice shall be included in
-    # all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be included
+    # in all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    # SOFTWARE.
-    #endregion License ############################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+    # USE OR OTHER DEALINGS IN THE SOFTWARE.
+    #endregion License ########################################################
 
-    #region DownloadLocationNotice #############################################
+    #region DownloadLocationNotice #########################################
     # The most up-to-date version of this script can be found on the author's
     # GitHub repository at:
     # https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #############################################
+    #endregion DownloadLocationNotice #########################################
 
     function Get-ReferenceToLastError {
-        #region FunctionHeader #################################################
+        #region FunctionHeader #############################################
         # Function returns $null if no errors on on the $error stack;
         # Otherwise, function returns a reference (memory pointer) to the last
         # error that occurred.
         #
-        # Version: 1.0.20241105.0
-        #endregion FunctionHeader #################################################
+        # Version: 1.0.20241211.0
+        #endregion FunctionHeader #############################################
 
-        #region License ########################################################
+        #region License ####################################################
         # Copyright (c) 2024 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining a
-        # copy of this software and associated documentation files (the
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
         # "Software"), to deal in the Software without restriction, including
         # without limitation the rights to use, copy, modify, merge, publish,
-        # distribute, sublicense, and/or sell copies of the Software, and to permit
-        # persons to whom the Software is furnished to do so, subject to the
-        # following conditions:
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
         #
-        # The above copyright notice and this permission notice shall be included
-        # in all copies or substantial portions of the Software.
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
         #
-        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-        # USE OR OTHER DEALINGS IN THE SOFTWARE.
-        #endregion License ########################################################
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ####################################################
 
-        #region DownloadLocationNotice #########################################
-        # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at:
+        #region DownloadLocationNotice #####################################
+        # The most up-to-date version of this script can be found on the
+        # author's GitHub repository at:
         # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #########################################
+        #endregion DownloadLocationNotice #####################################
 
         if ($Error.Count -gt 0) {
             return ([ref]($Error[0]))
@@ -106,50 +108,51 @@ function Join-PathSafely {
     }
 
     function Test-ErrorOccurred {
-        #region FunctionHeader #################################################
+        #region FunctionHeader #############################################
         # Function accepts two positional arguments:
         #
-        # The first argument is a reference (memory pointer) to the last error that
-        # had occurred prior to calling the command in question - that is, the
-        # command that we want to test to see if an error occurred.
+        # The first argument is a reference (memory pointer) to the last error
+        # that had occurred prior to calling the command in question - that is,
+        # the command that we want to test to see if an error occurred.
         #
-        # The second argument is a reference to the last error that had occurred
-        # as-of the completion of the command in question.
+        # The second argument is a reference to the last error that had
+        # occurred as-of the completion of the command in question.
         #
         # Function returns $true if it appears that an error occurred; $false
         # otherwise
         #
-        # Version: 1.0.20241105.0
-        #endregion FunctionHeader #################################################
+        # Version: 1.0.20241211.0
+        #endregion FunctionHeader #############################################
 
-        #region License ########################################################
+        #region License ####################################################
         # Copyright (c) 2024 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining a
-        # copy of this software and associated documentation files (the
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
         # "Software"), to deal in the Software without restriction, including
         # without limitation the rights to use, copy, modify, merge, publish,
-        # distribute, sublicense, and/or sell copies of the Software, and to permit
-        # persons to whom the Software is furnished to do so, subject to the
-        # following conditions:
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
         #
-        # The above copyright notice and this permission notice shall be included
-        # in all copies or substantial portions of the Software.
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
         #
-        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-        # USE OR OTHER DEALINGS IN THE SOFTWARE.
-        #endregion License ########################################################
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ####################################################
 
-        #region DownloadLocationNotice #########################################
-        # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at:
+        #region DownloadLocationNotice #####################################
+        # The most up-to-date version of this script can be found on the
+        # author's GitHub repository at:
         # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #########################################
+        #endregion DownloadLocationNotice #####################################
 
         # TO-DO: Validate input
 
@@ -162,7 +165,8 @@ function Join-PathSafely {
         } else {
             # One is $null, or both are $null
             # NOTE: ($args[0]) could be non-null, while ($args[1])
-            # could be null if $error was cleared; this does not indicate an error.
+            # could be null if $error was cleared; this does not indicate an
+            # error.
             # So:
             # If both are null, no error
             # If ($args[0]) is null and ($args[1]) is non-null, error
@@ -193,10 +197,10 @@ function Join-PathSafely {
     $actionPreferenceFormerErrorPreference = $global:ErrorActionPreference
 
     # Set ErrorActionPreference to SilentlyContinue; this will suppress error
-    # output. Terminating errors will not output anything, kick to the empty trap
-    # statement and then continue on. Likewise, non-terminating errors will also
-    # not output anything, but they do not kick to the trap statement; they simply
-    # continue on.
+    # output. Terminating errors will not output anything, kick to the empty
+    # trap statement and then continue on. Likewise, non-terminating errors
+    # will also not output anything, but they do not kick to the trap
+    # statement; they simply continue on.
     $global:ErrorActionPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
     # Attempt to join the path
