d6d89f40b4fde99a0b071a1bfa3032e06ff85a4d
Frank Lesniak <franklesniak@hotmail.com>
2024-12-17 10:33:44 -0600

    Update error handling functions to latest version

--- Get-ScriptingFileSystemObjectSafely.ps1
+++ Get-ScriptingFileSystemObjectSafely.ps1
@@ -41,7 +41,7 @@ function Get-ScriptingFileSystemObjectSafely {
     # FileSystemObject COM object. If the object is not created successfully, then
     # the referenced variable becomes undefined.
     #
-    # Version: 1.1.20241216.1
+    # Version: 1.1.20241217.0
 
     #region License ############################################################
     # Copyright (c) 2024 Frank Lesniak
@@ -71,42 +71,43 @@ function Get-ScriptingFileSystemObjectSafely {
 
     #region FunctionsToSupportErrorHandling ####################################
     function Get-ReferenceToLastError {
-        #region FunctionHeader #################################################
+        #region FunctionHeader #############################################
         # Function returns $null if no errors on on the $error stack;
         # Otherwise, function returns a reference (memory pointer) to the last
         # error that occurred.
         #
-        # Version: 1.0.20241105.0
-        #endregion FunctionHeader #################################################
+        # Version: 1.0.20241211.0
+        #endregion FunctionHeader #############################################
 
-        #region License ########################################################
+        #region License ####################################################
         # Copyright (c) 2024 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining a
-        # copy of this software and associated documentation files (the
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
         # "Software"), to deal in the Software without restriction, including
         # without limitation the rights to use, copy, modify, merge, publish,
-        # distribute, sublicense, and/or sell copies of the Software, and to permit
-        # persons to whom the Software is furnished to do so, subject to the
-        # following conditions:
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
         #
-        # The above copyright notice and this permission notice shall be included
-        # in all copies or substantial portions of the Software.
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
         #
-        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-        # USE OR OTHER DEALINGS IN THE SOFTWARE.
-        #endregion License ########################################################
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ####################################################
 
-        #region DownloadLocationNotice #########################################
-        # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at:
+        #region DownloadLocationNotice #####################################
+        # The most up-to-date version of this script can be found on the
+        # author's GitHub repository at:
         # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #########################################
+        #endregion DownloadLocationNotice #####################################
 
         if ($Error.Count -gt 0) {
             return ([ref]($Error[0]))
@@ -116,50 +117,51 @@ function Get-ScriptingFileSystemObjectSafely {
     }
 
     function Test-ErrorOccurred {
-        #region FunctionHeader #################################################
+        #region FunctionHeader #############################################
         # Function accepts two positional arguments:
         #
-        # The first argument is a reference (memory pointer) to the last error that
-        # had occurred prior to calling the command in question - that is, the
-        # command that we want to test to see if an error occurred.
+        # The first argument is a reference (memory pointer) to the last error
+        # that had occurred prior to calling the command in question - that is,
+        # the command that we want to test to see if an error occurred.
         #
-        # The second argument is a reference to the last error that had occurred
-        # as-of the completion of the command in question.
+        # The second argument is a reference to the last error that had
+        # occurred as-of the completion of the command in question.
         #
         # Function returns $true if it appears that an error occurred; $false
         # otherwise
         #
-        # Version: 1.0.20241105.0
-        #endregion FunctionHeader #################################################
+        # Version: 1.0.20241211.0
+        #endregion FunctionHeader #############################################
 
-        #region License ########################################################
+        #region License ####################################################
         # Copyright (c) 2024 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining a
-        # copy of this software and associated documentation files (the
+        # Permission is hereby granted, free of charge, to any person obtaining
+        # a copy of this software and associated documentation files (the
         # "Software"), to deal in the Software without restriction, including
         # without limitation the rights to use, copy, modify, merge, publish,
-        # distribute, sublicense, and/or sell copies of the Software, and to permit
-        # persons to whom the Software is furnished to do so, subject to the
-        # following conditions:
+        # distribute, sublicense, and/or sell copies of the Software, and to
+        # permit persons to whom the Software is furnished to do so, subject to
+        # the following conditions:
         #
-        # The above copyright notice and this permission notice shall be included
-        # in all copies or substantial portions of the Software.
+        # The above copyright notice and this permission notice shall be
+        # included in all copies or substantial portions of the Software.
         #
-        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-        # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-        # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-        # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-        # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-        # USE OR OTHER DEALINGS IN THE SOFTWARE.
-        #endregion License ########################################################
+        # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        # SOFTWARE.
+        #endregion License ####################################################
 
-        #region DownloadLocationNotice #########################################
-        # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at:
+        #region DownloadLocationNotice #####################################
+        # The most up-to-date version of this script can be found on the
+        # author's GitHub repository at:
         # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #########################################
+        #endregion DownloadLocationNotice #####################################
 
         # TO-DO: Validate input
 
@@ -172,7 +174,8 @@ function Get-ScriptingFileSystemObjectSafely {
         } else {
             # One is $null, or both are $null
             # NOTE: ($args[0]) could be non-null, while ($args[1])
-            # could be null if $error was cleared; this does not indicate an error.
+            # could be null if $error was cleared; this does not indicate an
+            # error.
             # So:
             # If both are null, no error
             # If ($args[0]) is null and ($args[1]) is non-null, error
