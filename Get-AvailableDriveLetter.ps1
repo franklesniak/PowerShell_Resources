d29f4a0c0f4b30c816d83349aa94ddec1925adb6
Frank Lesniak <franklesniak@hotmail.com>
2024-12-27 16:29:08 -0600

    Add comment-based help; force a return of an array, always

--- Get-AvailableDriveLetter.ps1
+++ Get-AvailableDriveLetter.ps1
@@ -1,33 +1,47 @@
 function Get-AvailableDriveLetter {
-    #region FunctionHeader #####################################################
+    # .SYNOPSIS
+    # Gets a list of available drive letters on the local system.
+    #
+    # .DESCRIPTION
     # This function evaluates the list of drive letters that are in use on the
     # local system and returns an array of those that are available. The list of
     # available drive letters is returned as an array of uppercase letters
     #
-    # The function returns an array of uppercase letters (strings) representing
-    # available drive letters
+    # .PARAMETER PSVersion
+    # This parameter is optional; if supplied, it must be the version number of the
+    # running version of PowerShell. If the version of PowerShell is already known,
+    # it can be passed in to this function to avoid the overhead of unnecessarily
+    # determining the version of PowerShell. If this parameter is not supplied, the
+    # function will determine the version of PowerShell that is running as part of
+    # its processing.
     #
-    # This function supports three parameters:
+    # .PARAMETER AssumeWindows
+    # By default, this function will determine if the running system is Windows or
+    # not. If this switch parameter is supplied, then the function will assume that
+    # the running system is Windows. This can be useful if you have already
+    # determined that the system is Windows and you want to avoid the overhead of
+    # determining the system type again.
     #
-    # Parameter 1: DoNotConsiderMappedDriveLettersAsInUse
+    # .PARAMETER DoNotConsiderMappedDriveLettersAsInUse
     # By default, if this function encounters a drive letter that is mapped to a
     # network share, it will consider that drive letter to be in use. However, if
     # this switch parameter is supplied, then mapped drives will be ignored and
     # their drive letters will be considered available.
     #
-    # Parameter 2: DoNotConsiderPSDriveLettersAsInUse
+    # .PARAMETER DoNotConsiderPSDriveLettersAsInUse
     # By default, if this function encounters a drive letter that is mapped to a
     # PowerShell drive, it will consider that drive letter to be in use. However,
     # if this switch parameter is supplied, then PowerShell drives will be ignored
     # and their drive letters will be considered available.
     #
-    # Parameter 3: ConsiderFloppyDriveLettersAsEligible
+    # .PARAMETER ConsiderFloppyDriveLettersAsEligible
     # By default, this function will not consider A: or B: drive letters as
     # available. If this switch parameter is supplied, then A: and B: drive letters
     # will be considered available if they are not in use.
     #
-    # Example:
-    # $arrAvailableDriveLetters = @(Get-AvailableDriveLetter)
+    # .EXAMPLE
+    # $arrAvailableDriveLetters = Get-AvailableDriveLetter
+    #
     # This example returns an array of available drive letters, excluding A: and B:
     # drive, and excluding drive letters that are mapped to network shares or
     # PowerShell drives (PSDrives).
@@ -38,11 +52,31 @@ function Get-AvailableDriveLetter {
     # To access the alphabetically-last available drive letter, use:
     # $arrAvailableDriveLetters[-1]
     #
-    # Note: it is conventional that A: and B: drives be reserved for floppy drives,
+    # .INPUTS
+    # None. You can't pipe objects to Get-AvailableDriveLetter.
+    #
+    # .OUTPUTS
+    # System.Char[]. Get-AvailableDriveLetter returns an array of uppercase letters
+    # (System.Char) representing available drive letters.
+    #
+    # Note that on non-Windows systems, this function will return an empty array
+    # because drive letters are a Windows-specific concept. It will also issue a
+    # warning to alert the user that the function is only supported on Windows.
+    #
+    # .NOTES
+    # It is conventional that A: and B: drives be reserved for floppy drives,
     # and that C: be reserved for the system drive.
     #
-    # Version 1.0.20241225.1
-    #endregion FunctionHeader #####################################################
+    # This function also supports the use of one positional parameter instead of
+    # named parameters. If the positional parameter is used intead of named
+    # parameters, then one positional parameters is required: it must be the
+    # version number of the running version of PowerShell. If the version of
+    # PowerShell is already known, it can be passed in to this function to avoid
+    # the overhead of unnecessarily determining the version of PowerShell. If this
+    # parameter is not supplied, the function will determine the version of
+    # PowerShell that is running as part of its processing.
+    #
+    # Version: 1.1.20241227.0
 
     #region License ############################################################
     # Copyright (c) 2024 Frank Lesniak
@@ -66,12 +100,9 @@ function Get-AvailableDriveLetter {
     # SOFTWARE.
     #endregion License ############################################################
 
-    #region DownloadLocationNotice #############################################
-    # The most up-to-date version of this script can be found on the author's
-    # GitHub repository at https://github.com/franklesniak/PowerShell_Resources
-    #endregion DownloadLocationNotice #############################################
-
     param (
+        [version]$PSVersion = ([version]'0.0'),
+        [switch]$AssumeWindows,
         [switch]$DoNotConsiderMappedDriveLettersAsInUse,
         [switch]$DoNotConsiderPSDriveLettersAsInUse,
         [switch]$ConsiderFloppyDriveLettersAsEligible
@@ -287,86 +318,123 @@ function Get-AvailableDriveLetter {
     }
 
     #region Process Input ######################################################
-    if ($DoNotConsiderMappedDriveLettersAsInUse.IsPresent -eq $true) {
-        $boolExcludeMappedDriveLetters = $false
+    if ($null -ne $PSVersion) {
+        if ($PSVersion -eq ([version]'0.0')) {
+            $versionPS = Get-PSVersion
+        } else {
+            $versionPS = $PSVersion
+        }
     } else {
-        $boolExcludeMappedDriveLetters = $true
+        $versionPS = Get-PSVersion
     }
 
-    if ($DoNotConsiderPSDriveLettersAsInUse.IsPresent -eq $true) {
-        $boolExcludePSDriveLetters = $false
-    } else {
-        $boolExcludePSDriveLetters = $true
+    $boolIsWindows = $null
+    if ($null -ne $AssumeWindows) {
+        if ($AssumeWindows.IsPresent -eq $true) {
+            $boolIsWindows = $true
+        }
+    }
+    if ($null -eq $boolIsWindows) {
+        $boolIsWindows = Test-Windows -PSVersion $versionPS
     }
 
-    if ($ConsiderFloppyDriveLettersAsEligible.IsPresent -eq $true) {
-        $boolExcludeFloppyDriveLetters = $false
-    } else {
-        $boolExcludeFloppyDriveLetters = $true
+    $boolExcludeMappedDriveLetters = $true
+    if ($null -ne $DoNotConsiderMappedDriveLettersAsInUse) {
+        if ($DoNotConsiderMappedDriveLettersAsInUse.IsPresent -eq $true) {
+            $boolExcludeMappedDriveLetters = $false
+        }
+    }
+
+    $boolExcludePSDriveLetters = $true
+    if ($null -ne $DoNotConsiderPSDriveLettersAsInUse) {
+        if ($DoNotConsiderPSDriveLettersAsInUse.IsPresent -eq $true) {
+            $boolExcludePSDriveLetters = $false
+        }
+    }
+
+    $boolExcludeFloppyDriveLetters = $true
+    if ($null -ne $ConsiderFloppyDriveLettersAsEligible) {
+        if ($ConsiderFloppyDriveLettersAsEligible.IsPresent -eq $true) {
+            $boolExcludeFloppyDriveLetters = $false
+        }
     }
     #endregion Process Input ######################################################
 
     $VerbosePreferenceAtStartOfFunction = $VerbosePreference
 
-    $versionPS = Get-PSVersion
+    if (-not $boolIsWindows) {
+        Write-Warning "Get-AvailableDriveLetter is only supported on Windows."
+        return , @()
+    } else {
+        # System is Windows
+        $arrAllPossibleLetters = @(65..90 | ForEach-Object { [char]$_ })
 
-    if ((Test-Windows -PSVersion $versionPS) -eq $true) {
-        $arrAllPossibleLetters = 65..90 | ForEach-Object { [char]$_ }
-
-        If ($versionPS.Major -ge 3) {
+        if ($versionPS.Major -ge 3) {
+            # Use Get-CimInstance
             $VerbosePreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
-            $arrUsedLogicalDriveLetters = Get-CimInstance -ClassName 'Win32_LogicalDisk' |
-                ForEach-Object { $_.DeviceID } | Where-Object { $_.Length -eq 2 } |
-                Where-Object { $_[1] -eq ':' } | ForEach-Object { $_.ToUpper() } |
-                ForEach-Object { $_[0] } | Where-Object { $arrAllPossibleLetters -contains $_ }
+            $arrUsedLogicalDriveLetters = @(Get-CimInstance -ClassName 'Win32_LogicalDisk' |
+                    ForEach-Object { $_.DeviceID } |
+                    Where-Object { $_.Length -eq 2 } |
+                    Where-Object { $_[1] -eq ':' } |
+                    ForEach-Object { $_.ToUpper() } |
+                    ForEach-Object { $_[0] } |
+                    Where-Object { $arrAllPossibleLetters -contains $_ })
             # fifth-, fourth-, and third-to-last bits of pipeline ensures that we
             # have a device ID like "C:"; second-to-last bit of pipeline strips off
             # the ':', leaving just the capital drive letter; last bit of pipeline
             # ensure that the drive letter is actually a letter; addresses legacy
-            # Netware edge cases
+            # Netware edge cases.
             $VerbosePreference = $VerbosePreferenceAtStartOfFunction
 
             if ($boolExcludeMappedDriveLetters -eq $true) {
                 $VerbosePreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
-                $arrUsedMappedDriveLetters = Get-CimInstance -ClassName 'Win32_NetworkConnection' |
-                    ForEach-Object { $_.LocalName } | Where-Object { $_.Length -eq 2 } |
-                    Where-Object { $_[1] -eq ':' } | ForEach-Object { $_.ToUpper() } |
-                    ForEach-Object { $_[0] } |
-                    Where-Object { $arrAllPossibleLetters -contains $_ }
+                $arrUsedMappedDriveLetters = @(Get-CimInstance -ClassName 'Win32_NetworkConnection' |
+                        ForEach-Object { $_.LocalName } |
+                        Where-Object { $_.Length -eq 2 } |
+                        Where-Object { $_[1] -eq ':' } |
+                        ForEach-Object { $_.ToUpper() } |
+                        ForEach-Object { $_[0] } |
+                        Where-Object { $arrAllPossibleLetters -contains $_ })
                 # fifth-, fourth-, and third-to-last bits of pipeline ensures that
                 # we have a LocalName like "C:"; second-to-last bit of pipeline
                 # strips off the ':', leaving just the capital drive letter; last
                 # bit of pipeline ensure that the drive letter is actually a
-                # letter; addresses legacy Netware edge cases
+                # letter; addresses legacy Netware edge cases.
                 $VerbosePreference = $VerbosePreferenceAtStartOfFunction
             } else {
                 $arrUsedMappedDriveLetters = $null
             }
         } else {
+            # Use Get-WmiObject
             $VerbosePreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
-            $arrUsedLogicalDriveLetters = Get-WmiObject -Class 'Win32_LogicalDisk' |
-                ForEach-Object { $_.DeviceID } | Where-Object { $_.Length -eq 2 } |
-                Where-Object { $_[1] -eq ':' } | ForEach-Object { $_.ToUpper() } |
-                ForEach-Object { $_[0] } | Where-Object { $arrAllPossibleLetters -contains $_ }
+            $arrUsedLogicalDriveLetters = @(Get-WmiObject -Class 'Win32_LogicalDisk' |
+                    ForEach-Object { $_.DeviceID } |
+                    Where-Object { $_.Length -eq 2 } |
+                    Where-Object { $_[1] -eq ':' } |
+                    ForEach-Object { $_.ToUpper() } |
+                    ForEach-Object { $_[0] } |
+                    Where-Object { $arrAllPossibleLetters -contains $_ })
             # fifth-, fourth-, and third-to-last bits of pipeline ensures that we
             # have a device ID like "C:"; second-to-last bit of pipeline strips off
             # the ':', leaving just the capital drive letter; last bit of pipeline
             # ensure that the drive letter is actually a letter; addresses legacy
-            # Netware edge cases
+            # Netware edge cases.
             $VerbosePreference = $VerbosePreferenceAtStartOfFunction
 
             if ($boolExcludeMappedDriveLetters -eq $true) {
                 $VerbosePreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
-                $arrUsedMappedDriveLetters = Get-WmiObject -Class 'Win32_NetworkConnection' |
-                    ForEach-Object { $_.LocalName } | Where-Object { $_.Length -eq 2 } |
-                    Where-Object { $_[1] -eq ':' } | ForEach-Object { $_.ToUpper() } |
-                    ForEach-Object { $_[0] } |
-                    Where-Object { $arrAllPossibleLetters -contains $_ }
+                $arrUsedMappedDriveLetters = @(Get-WmiObject -Class 'Win32_NetworkConnection' |
+                        ForEach-Object { $_.LocalName } |
+                        Where-Object { $_.Length -eq 2 } |
+                        Where-Object { $_[1] -eq ':' } |
+                        ForEach-Object { $_.ToUpper() } |
+                        ForEach-Object { $_[0] } |
+                        Where-Object { $arrAllPossibleLetters -contains $_ })
                 # fifth-, fourth-, and third-to-last bits of pipeline ensures that
                 # we have a LocalName like "C:"; second-to-last bit of pipeline
                 # strips off the ':', leaving just the capital drive letter; last
                 # bit of pipeline ensure that the drive letter is actually a
-                # letter; addresses legacy Netware edge cases
+                # letter; addresses legacy Netware edge cases.
                 $VerbosePreference = $VerbosePreferenceAtStartOfFunction
             } else {
                 $arrUsedMappedDriveLetters = $null
@@ -374,13 +442,14 @@ function Get-AvailableDriveLetter {
         }
 
         if ($boolExcludePSDriveLetters -eq $true) {
-            $arrUsedPSDriveLetters = Get-PSDrive | ForEach-Object { $_.Name } |
-                Where-Object { $_.Length -eq 1 } | ForEach-Object { $_.ToUpper() } |
-                Where-Object { $arrAllPossibleLetters -contains $_ }
+            $arrUsedPSDriveLetters = @(Get-PSDrive | ForEach-Object { $_.Name } |
+                    Where-Object { $_.Length -eq 1 } |
+                    ForEach-Object { $_.ToUpper() } |
+                    Where-Object { $arrAllPossibleLetters -contains $_ })
             # Checking for a length of 1 strips out most PSDrives that are not
             # drive letters; making sure that each item in the resultant set
             # matches something in $arrAllPossibleLetters filters out edge cases,
-            # like a PSDrive named '1'
+            # like a PSDrive named '1'.
         } else {
             $arrUsedPSDriveLetters = $null
         }
@@ -391,12 +460,33 @@ function Get-AvailableDriveLetter {
             $arrFloppyDriveLetters = $null
         }
 
-        $arrAllPossibleLetters | Where-Object { $arrUsedLogicalDriveLetters -notcontains $_ } |
-            Where-Object { $arrUsedMappedDriveLetters -notcontains $_ } |
-            Where-Object { $arrUsedPSDriveLetters -notcontains $_ } |
-            Where-Object { $arrFloppyDriveLetters -notcontains $_ } |
-            Where-Object { $arrBlacklistedDriveLetters -notcontains $_ }
-    } else {
-        Write-Warning "This function is only supported on Windows."
+        $arrResult = @($arrAllPossibleLetters |
+                Where-Object { $arrUsedLogicalDriveLetters -notcontains $_ } |
+                Where-Object { $arrUsedMappedDriveLetters -notcontains $_ } |
+                Where-Object { $arrUsedPSDriveLetters -notcontains $_ } |
+                Where-Object { $arrFloppyDriveLetters -notcontains $_ } |
+                Where-Object { $arrBlacklistedDriveLetters -notcontains $_ })
+
+        # The following code forces the function to return an array, always, even
+        # when there are zero or one elements in the array.
+        $intElementCount = 1
+        if ($null -ne $arrResult) {
+            if ($arrResult.GetType().FullName.Contains('[]')) {
+                if (($arrResult.Count -ge 2) -or ($arrResult.Count -eq 0)) {
+                    $intElementCount = $arrResult.Count
+                }
+            }
+        }
+        $strLowercaseFunctionName = $MyInvocation.InvocationName.ToLower()
+        $boolArrayEncapsulation = $MyInvocation.Line.ToLower().Contains('@(' + $strLowercaseFunctionName + ')') -or $MyInvocation.Line.ToLower().Contains('@(' + $strLowercaseFunctionName + ' ')
+        if ($boolArrayEncapsulation) {
+            return $arrResult
+        } elseif ($intElementCount -eq 0) {
+            return , @()
+        } elseif ($intElementCount -eq 1) {
+            return , (, ($arrResult[0]))
+        } else {
+            return $arrResult
+        }
     }
 }
