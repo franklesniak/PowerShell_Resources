839af879f067c41f041c6c9bc27572891ffe1795
Frank Lesniak <franklesniak@hotmail.com>
2024-12-31 14:11:32 -0600

    Update embedded Join-PathSafely; add notes about positional parameters

--- Wait-PathToBeReady.ps1
+++ Wait-PathToBeReady.ps1
@@ -26,7 +26,7 @@ function Wait-PathToBeReady {
     # the parent path to be used in the Join-Path operation. If no child path is
     # specified in the parameter ChildItemPath, then the contents of the Path parameter
     # will populated into the variable referenced in the parameter
-    # ReferenceToJoinedPath
+    # ReferenceToJoinedPath.
     #
     # .PARAMETER ChildItemPath
     # This parameter is a string containing the child path to be used in the Join-Path
@@ -57,8 +57,48 @@ function Wait-PathToBeReady {
     #
     # .OUTPUTS
     # A boolean value indiciating whether the path is available
-
-    # Version: 1.0.20241223.0
+    #
+    # .NOTES
+    # This function also supports the use of positional parameters instead of named
+    # parameters. If positional parameters are used intead of named parameters,
+    # then three to five positional parameters are required:
+    #
+    # The first positional parameter is a memory reference to a string variable
+    # that will be populated with the joined path (parent path + child path). If no
+    # child path was specified, then the parent path will be populated in the
+    # referenced variable.
+    #
+    # The second positional parameter is a memory reference to a boolean variable
+    # that indicates whether or not the Get-PSDrive workaround should be used. If
+    # the Get-PSDrive workaround is used, then the function will use the
+    # Get-PSDrive cmdlet to refresh PowerShell's "understanding" of the available
+    # drive letters. This variable is passed by reference to ensure that this
+    # function can set the variable to $true if the Get-PSDrive workaround is
+    # successful - which improves performance of subsequent runs.
+    #
+    # The third positional parameter is a string containing the path to be tested
+    # for availability, and the parent path to be used in the Join-Path operation.
+    # If no child path is specified in the fourth positional parameter, then the
+    # contents of the Path parameter will populated into the variable referenced in
+    # the first positional parameter.
+    #
+    # The fourth positional parameter is optional; if supplied, it is a string
+    # containing the child path to be used in the Join-Path operation. If it is not
+    # specified, or if it contains $null or a blank string, then the path specified
+    # by the third positional parameter will be populated into the variable
+    # referenced in the first positional parameter. However, if this fourth
+    # positional parameter contains a string containing data, then the path
+    # specified by the third positional parameter will be used as the parent path
+    # in the Join-Path operation, and this fourth positional parameter will be used
+    # as the child path in the Join-Path operation. The joined path will be
+    # populated into the variable referenced in the first positional parameter.
+    #
+    # The fifth positional parameter is optional; if supplied, it is the maximum
+    # amount of seconds to wait for the path to be ready. If the path is not ready
+    # within this time, then the function will return $false. By default, this
+    # parameter is set to 10 seconds.
+    #
+    # Version: 1.0.20241231.0
 
     #region License ############################################################
     # Copyright (c) 2024 Frank Lesniak
@@ -98,38 +138,67 @@ function Wait-PathToBeReady {
     )
 
     function Join-PathSafely {
-        #region FunctionHeader #################################################
-        # Combines two paths into a single path. This function is intended to be
-        # used in situations where the Join-Path cmdlet may fail due to a variety
-        # of reasons. This function is designed to suppress errors and return a
-        # boolean value indicating whether the operation was successful.
+        # .SYNOPSIS
+        # Joins two path parts together and suppresses any errors that may occur.
         #
-        # Three positional arguments are required:
+        # .DESCRIPTION
+        # Combines two paths parts (parent and child) into a single path. This
+        # function is intended to be used in situations where the Join-Path cmdlet
+        # may fail due to a variety of reasons. This function is designed to
+        # suppress errors and return a boolean value indicating whether the
+        # operation was successful.
         #
-        # The first argument is a reference to a string object that will be
-        # populated with the joined path (parent path + child path). If the
+        # .PARAMETER ReferenceToJoinedPath
+        # This parameter is required; it is a reference to a string object that
+        # will be populated with the joined path (parent path + child path). If the
         # operation was successful, the referenced string object will be populated
         # with the joined path. If the operation was unsuccessful, the referenced
-        # string will be left unchanged.
+        # string is undefined.
         #
-        # The second argument is a string representing the parent part of the path.
+        # .PARAMETER ParentPath
+        # This parameter is required; it is a string representing the parent part
+        # of the path.
         #
-        # The third argument is the child part of the path.
+        # .PARAMETER ChildPath
+        # This parameter is required; it is the child part of the path.
         #
-        # The function returns a boolean value indicating whether the operation was
-        # successful. If the operation was successful, the joined path will be
-        # populated in the string object referenced in the first argument. If the
-        # operation was unsuccessful, the referenced string object will be left
-        # unchanged.
+        # .EXAMPLE
+        # $strParentPartOfPath = 'Z:'
+        # $strChildPartOfPath = '####FAKE####'
+        # $strJoinedPath = $null
+        # $boolSuccess = Join-PathSafely -ReferenceToJoinedPath ([ref]$strJoinedPath) -ParentPath $strParentPartOfPath -ChildPath $strChildPartOfPath
         #
-        # Example usage:
+        # .EXAMPLE
         # $strParentPartOfPath = 'Z:'
         # $strChildPartOfPath = '####FAKE####'
         # $strJoinedPath = $null
         # $boolSuccess = Join-PathSafely ([ref]$strJoinedPath) $strParentPartOfPath $strChildPartOfPath
         #
-        # Version 1.0.20241223.0
-        #endregion FunctionHeader #################################################
+        # .INPUTS
+        # None. You can't pipe objects to Join-PathSafely.
+        #
+        # .OUTPUTS
+        # System.Boolean. Join-PathSafely returns a boolean value indiciating
+        # whether the process completed successfully. $true means the process
+        # completed successfully; $false means there was an error.
+        #
+        # .NOTES
+        # This function also supports the use of positional parameters instead of
+        # named parameters. If positional parameters are used intead of named
+        # parameters, then three positional parameters are required:
+        #
+        # The first positional parameter is a reference to a string object that
+        # will be populated with the joined path (parent path + child path). If the
+        # operation was successful, the referenced string object will be populated
+        # with the joined path. If the operation was unsuccessful, the referenced
+        # string is undefined.
+        #
+        # The second positional parameter is a string representing the parent part
+        # of the path.
+        #
+        # The third positional parameter is the child part of the path.
+        #
+        # Version: 2.0.20241231.0
 
         #region License ########################################################
         # Copyright (c) 2024 Frank Lesniak
@@ -154,12 +223,13 @@ function Wait-PathToBeReady {
         # USE OR OTHER DEALINGS IN THE SOFTWARE.
         #endregion License ########################################################
 
-        #region DownloadLocationNotice #########################################
-        # The most up-to-date version of this script can be found on the author's
-        # GitHub repository at:
-        # https://github.com/franklesniak/PowerShell_Resources
-        #endregion DownloadLocationNotice #########################################
+        param (
+            [ref]$ReferenceToJoinedPath = ([ref]$null),
+            [string]$ParentPath = '',
+            [string]$ChildPath = ''
+        )
 
+        #region FunctionsToSupportErrorHandling ################################
         function Get-ReferenceToLastError {
             # .SYNOPSIS
             # Gets a reference (memory pointer) to the last error that occurred.
@@ -421,22 +491,25 @@ function Wait-PathToBeReady {
 
             return $boolErrorOccurred
         }
+        #endregion FunctionsToSupportErrorHandling ################################
 
         trap {
             # Intentionally left empty to prevent terminating errors from halting
             # processing
         }
 
-        $refOutputJoinedPath = $args[0]
-        $strParentPartOfPath = $args[1]
-        $strChildPartOfPath = $args[2]
-
-        $strJoinedPath = $null
+        #region Process Input ##################################################
+        if ([string]::IsNullOrEmpty($ParentPath)) {
+            Write-Warning "In the function Join-PathSafely(), the ParentPath parameter is required and cannot be null or empty."
+            return $false
+        }
+        #endregion Process Input ##################################################
 
         # Retrieve the newest error on the stack prior to doing work
         $refLastKnownError = Get-ReferenceToLastError
 
-        # Store current error preference; we will restore it after we do our work
+        # Store current error preference; we will restore it after we do the work
+        # of this function
         $actionPreferenceFormerErrorPreference = $global:ErrorActionPreference
 
         # Set ErrorActionPreference to SilentlyContinue; this will suppress error
@@ -447,7 +520,7 @@ function Wait-PathToBeReady {
         $global:ErrorActionPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
 
         # Attempt to join the path
-        $strJoinedPath = Join-Path $strParentPartOfPath $strChildPartOfPath
+        ($ReferenceToJoinedPath.Value) = Join-Path -Path $ParentPath -ChildPath $ChildPath
 
         # Restore the former error preference
         $global:ErrorActionPreference = $actionPreferenceFormerErrorPreference
@@ -456,9 +529,10 @@ function Wait-PathToBeReady {
         $refNewestCurrentError = Get-ReferenceToLastError
 
         if (Test-ErrorOccurred -ReferenceToEarlierError $refLastKnownError -ReferenceToLaterError $refNewestCurrentError) {
+            # Error occurred; return failure indicator:
             return $false
         } else {
-            $refOutputJoinedPath.Value = $strJoinedPath
+            # No error occurred; return success indicator:
             return $true
         }
     }
@@ -500,7 +574,7 @@ function Wait-PathToBeReady {
     while ($doubleSecondsCounter -le $MaximumWaitTimeInSeconds -and $boolFunctionReturn -eq $false) {
         if (Test-Path $Path) {
             $strJoinedPath = $null
-            $boolSuccess = Join-PathSafely ([ref]$strJoinedPath) $Path $strWorkingChildItemPath
+            $boolSuccess = Join-PathSafely -ReferenceToJoinedPath ([ref]$strJoinedPath) -ParentPath $Path -ChildPath $strWorkingChildItemPath
 
             if ($boolSuccess -eq $false) {
                 Start-Sleep 0.2
@@ -531,7 +605,7 @@ function Wait-PathToBeReady {
                 while ($doubleSecondsCounter -le $MaximumWaitTimeInSeconds -and $boolFunctionReturn -eq $false) {
                     if (Test-Path $Path) {
                         $strJoinedPath = $null
-                        $boolSuccess = Join-PathSafely ([ref]$strJoinedPath) $Path $strWorkingChildItemPath
+                        $boolSuccess = Join-PathSafely -ReferenceToJoinedPath ([ref]$strJoinedPath) -ParentPath $Path -ChildPath $strWorkingChildItemPath
 
                         if ($boolSuccess -eq $false) {
                             Start-Sleep 0.2
