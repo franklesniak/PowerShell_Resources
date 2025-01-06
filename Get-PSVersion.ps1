b712af5814a38b189727da6a5f4de616fe5770ec
Frank Lesniak <franklesniak@hotmail.com>
2025-01-06 12:29:47 -0600

    Shorten max comment line width to 76 characters

--- Get-PSVersion.ps1
+++ Get-PSVersion.ps1
@@ -9,14 +9,14 @@ function Get-PSVersion {
     # On versions of PowerShell greater than or equal to version 2.0, this
     # function returns the equivalent of $PSVersionTable.PSVersion
     #
-    # PowerShell 1.0 does not have a $PSVersionTable variable, so this function
-    # returns [version]('1.0') on PowerShell 1.0.
+    # PowerShell 1.0 does not have a $PSVersionTable variable, so this
+    # function returns [version]('1.0') on PowerShell 1.0.
     #
     # .EXAMPLE
     # $versionPS = Get-PSVersion
-    # # $versionPS now contains the version of PowerShell that is running. On
-    # # versions of PowerShell greater than or equal to version 2.0, this
-    # # function returns the equivalent of $PSVersionTable.PSVersion
+    # # $versionPS now contains the version of PowerShell that is running.
+    # # On versions of PowerShell greater than or equal to version 2.0,
+    # # this function returns the equivalent of $PSVersionTable.PSVersion.
     #
     # .INPUTS
     # None. You can't pipe objects to Get-PSVersion.
@@ -26,30 +26,31 @@ function Get-PSVersion {
     # the version of PowerShell that is running.
     #
     # .NOTES
-    # Version: 1.0.20241225.0
+    # Version: 1.0.20250106.0
 
-    #region License ########################################################
-    # Copyright (c) 2024 Frank Lesniak
+    #region License ####################################################
+    # Copyright (c) 2025 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a
-    # copy of this software and associated documentation files (the
+    # Permission is hereby granted, free of charge, to any person obtaining
+    # a copy of this software and associated documentation files (the
     # "Software"), to deal in the Software without restriction, including
     # without limitation the rights to use, copy, modify, merge, publish,
-    # distribute, sublicense, and/or sell copies of the Software, and to permit
-    # persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # distribute, sublicense, and/or sell copies of the Software, and to
+    # permit persons to whom the Software is furnished to do so, subject to
+    # the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included
-    # in all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be
+    # included in all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-    # USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ########################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+    # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+    # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ####################################################
 
     if (Test-Path variable:\PSVersionTable) {
         return ($PSVersionTable.PSVersion)
