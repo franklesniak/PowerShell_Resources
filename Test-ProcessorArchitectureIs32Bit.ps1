22d3715ca210ecbab3b0edec7f4c99db1246c352
Frank Lesniak <franklesniak@hotmail.com>
2025-04-06 12:36:53 -0500

    Shorten comment width to 76 characters

--- Test-ProcessorArchitectureIs32Bit.ps1
+++ Test-ProcessorArchitectureIs32Bit.ps1
@@ -1,23 +1,23 @@
 function Test-ProcessorArchitectureIs32Bit {
     # .SYNOPSIS
-    # Tests a string that contains the processor architecture to determine if
-    # it is 32-bit.
+    # Tests a string that contains the processor architecture to determine
+    # if it is 32-bit.
     #
     # .DESCRIPTION
-    # This function tests a string that contains the processor architecture to
-    # determine if it is 32-bit. It updates a reference to a boolean that
-    # indicates whether the processor architecture is 32-bit. It returns an
-    # integer indicating whether the process completed successfully. 0 means
-    # the process completed successfully; any other value means there was an
-    # error.
+    # This function tests a string that contains the processor architecture
+    # to determine if it is 32-bit. It updates a reference to a boolean
+    # that indicates whether the processor architecture is 32-bit. It
+    # returns an integer indicating whether the process completed
+    # successfully. 0 means the process completed successfully; any other
+    # value means there was an error.
     #
     # .PARAMETER ReferenceToProcessorArchitectureIs32Bit
-    # This parameter is required; it is a reference to a boolean that will be
-    # used to store whether or not the processor architecture is 32-bit. This
-    # parameter is passed by reference, so the value of the boolean will be
-    # updated in the calling function. The value of this parameter is set to
-    # $true if the processor architecture is 32-bit; otherwise, it is set to
-    # $false.
+    # This parameter is required; it is a reference to a boolean that will
+    # be used to store whether or not the processor architecture is 32-bit.
+    # This parameter is passed by reference, so the value of the boolean
+    # will be updated in the calling function. The value of this parameter
+    # is set to $true if the processor architecture is 32-bit; otherwise,
+    # it is set to $false.
     #
     # .PARAMETER ProcessorArchitecture
     # This parameter is required; it is a string representing the processor
@@ -55,72 +55,76 @@ function Test-ProcessorArchitectureIs32Bit {
     # None. You can't pipe objects to Test-ProcessorArchitectureIs32Bit.
     #
     # .OUTPUTS
-    # System.Int32. Test-ProcessorArchitectureIs32Bit returns an integer value
-    # indiciating whether the process completed successfully. 0 means the
-    # processor architecture was evaluated successfully; any other number
-    # indicates an error occurred.
+    # System.Int32. Test-ProcessorArchitectureIs32Bit returns an integer
+    # value indiciating whether the process completed successfully. 0 means
+    # the processor architecture was evaluated successfully; any other
+    # number indicates an error occurred.
     #
     # .NOTES
-    # This function also supports the use of positional parameters instead of
-    # named parameters. If positional parameters are used instead of named
-    # parameters, then two positional parameters are required:
+    # This function also supports the use of positional parameters instead
+    # of named parameters. If positional parameters are used instead of
+    # named parameters, then two positional parameters are required:
     #
-    # The first positional parameter is a reference to a boolean that will be
-    # used to store whether or not the processor architecture is 32-bit. This
-    # parameter is passed by reference, so the value of the boolean will be
-    # updated in the calling function. The value of this parameter is set to
-    # $true if the processor architecture is 32-bit; otherwise, it is set to
-    # $false.
+    # The first positional parameter is a reference to a boolean that will
+    # be used to store whether or not the processor architecture is 32-bit.
+    # This parameter is passed by reference, so the value of the boolean
+    # will be updated in the calling function. The value of this parameter
+    # is set to $true if the processor architecture is 32-bit; otherwise,
+    # it is set to $false.
     #
-    # The second positional parameter is a string representing the processor
-    # architecture to be evaluated (e.g., "ARM64", "x86", "AMD64").
+    # The second positional parameter is a string representing the
+    # processor architecture to be evaluated (e.g., "ARM64", "x86",
+    # "AMD64").
     #
-    # Version: 1.0.20250406.0
+    # Version: 1.0.20250406.1
 
-    #region License ########################################################
+    #region License ####################################################
     # Copyright (c) 2025 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a
-    # copy of this software and associated documentation files (the
+    # Permission is hereby granted, free of charge, to any person obtaining
+    # a copy of this software and associated documentation files (the
     # "Software"), to deal in the Software without restriction, including
     # without limitation the rights to use, copy, modify, merge, publish,
-    # distribute, sublicense, and/or sell copies of the Software, and to permit
-    # persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # distribute, sublicense, and/or sell copies of the Software, and to
+    # permit persons to whom the Software is furnished to do so, subject to
+    # the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included
-    # in all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be
+    # included in all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-    # USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ########################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+    # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+    # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ####################################################
 
-    #region Acknowledgements ###############################################
-    # Microsoft, for providing a current reference on the SYSTEM_INFO struct,
-    # used by the GetSystemInfo Win32 function. This reference does not show
-    # the exact text of the PROCESSOR_ARCHITECTURE environment variable, but
-    # shows the universe of what's possible on a core system API:
+    #region Acknowledgements ###########################################
+    # Microsoft, for providing a current reference on the SYSTEM_INFO
+    # struct, used by the GetSystemInfo Win32 function. This reference does
+    # not show the exact text of the PROCESSOR_ARCHITECTURE environment
+    # variable, but shows the universe of what's possible on a core system
+    # API:
     # https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/ns-sysinfoapi-system_info#members
     #
-    # Microsoft, for including in the MSDN Library Jan 2003 information on this
-    # same SYSTEM_INFO struct that pre-dates Windows 2000 and enumerates
-    # additional processor architectures (MIPS, ALPHA, PowerPC, IA32_ON_WIN64).
-    # The MSDN Library Jan 2003 also lists SHX and ARM, explains nuiances in
-    # accessing environment variables on pre-Windows 2000 operating systems
-    # (namely that VBScript in Windows 9x can only access per-process
-    # environment variables), and that the PROCESSOR_ARCHITECTURE system
-    # environment variable is not available on Windows 98/ME.
+    # Microsoft, for including in the MSDN Library Jan 2003 information on
+    # this same SYSTEM_INFO struct that pre-dates Windows 2000 and
+    # enumerates additional processor architectures (MIPS, ALPHA, PowerPC,
+    # IA32_ON_WIN64). The MSDN Library Jan 2003 also lists SHX and ARM,
+    # explains nuiances in accessing environment variables on pre-Windows
+    # 2000 operating systems (namely that VBScript in Windows 9x can only
+    # access per-process environment variables), and that the
+    # PROCESSOR_ARCHITECTURE system environment variable is not available
+    # on Windows 98/ME.
     # (link unavailable, check Internet Archive for source)
     #
     # "guga" for the first post in this thread that tipped me off to the
     # SYSTEM_INFO struct and additional architectures:
     # http://masm32.com/board/index.php?topic=3401.0
-    #endregion Acknowledgements ###############################################
+    #endregion Acknowledgements ###########################################
 
     param (
         [ref]$ReferenceToProcessorArchitectureIs32Bit = ([ref]$null),
