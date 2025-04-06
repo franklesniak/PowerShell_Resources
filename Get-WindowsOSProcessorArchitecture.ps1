7fb7fbf3c7d623f3e579bb5d4c88932df35957ee
Frank Lesniak <franklesniak@hotmail.com>
2025-04-06 12:28:15 -0500

    Shorten comment width to 76 characters

--- Get-WindowsOSProcessorArchitecture.ps1
+++ Get-WindowsOSProcessorArchitecture.ps1
@@ -1,14 +1,14 @@
 function Get-WindowsOSProcessorArchitecture {
     # .SYNOPSIS
-    # Determines the processor architecture (instruction set) of the operating
-    # system.
+    # Determines the processor architecture (instruction set) of the
+    # operating system.
     #
     # .DESCRIPTION
-    # This function determines the processor architecture (instruction set) of
-    # the operating system. It reads this value from the registry and returns
-    # it via a referenced string object. The processor architecture is the
-    # instruction set that the operating system is using to run. On Windows
-    # versions that support PowerShell, known values are:
+    # This function determines the processor architecture (instruction set)
+    # of the operating system. It reads this value from the registry and
+    # returns it via a referenced string object. The processor architecture
+    # is the instruction set that the operating system is using to run. On
+    # Windows versions that support PowerShell, known values are:
     #
     # - x86 (i.e., 32-bit x86)
     # - AMD64 (i.e., x86-64 or 64-bit x86)
@@ -17,10 +17,10 @@ function Get-WindowsOSProcessorArchitecture {
     # - IA64 (i.e., Itanium)
     #
     # .PARAMETER ReferenceToOSProcessorArchitecture
-    # This parameter is required; it is a reference to a string that will be
-    # used to store output if the function completes successfully. The string
-    # will be set to the processor architecture (instruction set) of the
-    # operating system.
+    # This parameter is required; it is a reference to a string that will
+    # be used to store output if the function completes successfully. The
+    # string will be set to the processor architecture (instruction set) of
+    # the operating system.
     #
     # .EXAMPLE
     # $strOSProcessorArchitecture = ''
@@ -35,65 +35,68 @@ function Get-WindowsOSProcessorArchitecture {
     #
     # .OUTPUTS
     # System.Boolean. Get-WindowsOSProcessorArchitecture returns a boolean
-    # value indiciating whether the operating system processor architecture was
-    # retrieved successfully. $true means the operating system processor
-    # architecture was determined successfully; $false means there was an
-    # error.
+    # value indiciating whether the operating system processor architecture
+    # was retrieved successfully. $true means the operating system
+    # processor architecture was determined successfully; $false means
+    # there was an error.
     #
     # .NOTES
-    # This function also supports the use of a positional parameter instead of
-    # a named parameter. If a positional parameter is used instead of a named
-    # parameter, then exactly one positional parameter is required: a reference
-    # to a string that will be used to store output if the function completes
-    # successfully. The string will be set to the processor architecture
-    # (instruction set) of the operating system.
+    # This function also supports the use of a positional parameter instead
+    # of a named parameter. If a positional parameter is used instead of a
+    # named parameter, then exactly one positional parameter is required: a
+    # reference to a string that will be used to store output if the
+    # function completes successfully. The string will be set to the
+    # processor architecture (instruction set) of the operating system.
     #
-    # Version: 1.0.20250406.0
+    # Version: 1.0.20250406.1
 
-    #region License ########################################################
+    #region License ####################################################
     # Copyright (c) 2025 Frank Lesniak
     #
-    # Permission is hereby granted, free of charge, to any person obtaining a
-    # copy of this software and associated documentation files (the
+    # Permission is hereby granted, free of charge, to any person obtaining
+    # a copy of this software and associated documentation files (the
     # "Software"), to deal in the Software without restriction, including
     # without limitation the rights to use, copy, modify, merge, publish,
-    # distribute, sublicense, and/or sell copies of the Software, and to permit
-    # persons to whom the Software is furnished to do so, subject to the
-    # following conditions:
+    # distribute, sublicense, and/or sell copies of the Software, and to
+    # permit persons to whom the Software is furnished to do so, subject to
+    # the following conditions:
     #
-    # The above copyright notice and this permission notice shall be included
-    # in all copies or substantial portions of the Software.
+    # The above copyright notice and this permission notice shall be
+    # included in all copies or substantial portions of the Software.
     #
-    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-    # OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
-    # NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-    # DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
-    # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
-    # USE OR OTHER DEALINGS IN THE SOFTWARE.
-    #endregion License ########################################################
+    # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+    # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+    # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    # SOFTWARE.
+    #endregion License ####################################################
 
-    #region Acknowledgements ###############################################
-    # Microsoft, for providing a current reference on the SYSTEM_INFO struct,
-    # used by the GetSystemInfo Win32 function. This reference does not show
-    # the exact text of the PROCESSOR_ARCHITECTURE environment variable, but
-    # shows the universe of what's possible on a core system API:
+    #region Acknowledgements ###########################################
+    # Microsoft, for providing a current reference on the SYSTEM_INFO
+    # struct, used by the GetSystemInfo Win32 function. This reference does
+    # not show the exact text of the PROCESSOR_ARCHITECTURE environment
+    # variable, but shows the universe of what's possible on a core system
+    # API:
     # https://docs.microsoft.com/en-us/windows/win32/api/sysinfoapi/ns-sysinfoapi-system_info#members
     #
-    # Microsoft, for including in the MSDN Library Jan 2003 information on this
-    # same SYSTEM_INFO struct that pre-dates Windows 2000 and enumerates
-    # additional processor architectures (MIPS, ALPHA, PowerPC, IA32_ON_WIN64).
-    # The MSDN Library Jan 2003 also lists SHX and ARM, explains nuiances in
-    # accessing environment variables on pre-Windows 2000 operating systems
-    # (namely that VBScript in Windows 9x can only access per-process
-    # environment variables), and that the PROCESSOR_ARCHITECTURE system
-    # environment variable is not available on Windows 98/ME.
+    # Microsoft, for including in the MSDN Library Jan 2003 information on
+    # this same SYSTEM_INFO struct that pre-dates Windows 2000 and
+    # enumerates additional processor architectures (MIPS, ALPHA, PowerPC,
+    # IA32_ON_WIN64). The MSDN Library Jan 2003 also lists SHX and ARM,
+    # explains nuiances in accessing environment variables on pre-Windows
+    # 2000 operating systems (namely that VBScript in Windows 9x can only
+    # access per-process environment variables), and that the
+    # PROCESSOR_ARCHITECTURE system environment variable is not available
+    # on Windows 98/ME.
     # (link unavailable, check Internet Archive for source)
     #
     # "guga" for the first post in this thread that tipped me off to the
     # SYSTEM_INFO struct and additional architectures:
     # http://masm32.com/board/index.php?topic=3401.0
-    #endregion Acknowledgements ###############################################
+    #endregion Acknowledgements ###########################################
 
     param (
         [ref]$ReferenceToOSProcessorArchitecture = ([ref]$null)
@@ -105,41 +108,42 @@ function Get-WindowsOSProcessorArchitecture {
         # registry.
         #
         # .DESCRIPTION
-        # This function tests to determine whether a registry value exists in
-        # the Windows registry. It returns a boolean value indicating whether
-        # the registry value exists. $true indicates the registry value exists;
-        # $false indicates the registry value does not exist.
+        # This function tests to determine whether a registry value exists
+        # in the Windows registry. It returns a boolean value indicating
+        # whether the registry value exists. $true indicates the registry
+        # value exists; $false indicates the registry value does not exist.
         #
         # .PARAMETER RefPathToRegistryKey
         # Either this parameter or PathToRegistryKey are required; if
-        # RefPathToRegistryKey is specified, it is a reference to a string that
-        # contains the path to the registry key. If this parameter is not
-        # specified, then PathToRegistryKey must be specified. If both are
-        # specified, then RefPathToRegistryKey takes precedence over
+        # RefPathToRegistryKey is specified, it is a reference to a string
+        # that contains the path to the registry key. If this parameter is
+        # not specified, then PathToRegistryKey must be specified. If both
+        # are specified, then RefPathToRegistryKey takes precedence over
         # PathToRegistryKey.
         #
         # .PARAMETER PathToRegistryKey
         # Either this parameter or RefPathToRegistryKey are required; if
-        # PathToRegistryKey is specified, it is a string that contains the path
-        # to the registry key. If this parameter is not specified, then
-        # RefPathToRegistryKey must be specified. If both are specified, then
-        # RefPathToRegistryKey takes precedence over PathToRegistryKey.
+        # PathToRegistryKey is specified, it is a string that contains the
+        # path to the registry key. If this parameter is not specified,
+        # then RefPathToRegistryKey must be specified. If both are
+        # specified, then RefPathToRegistryKey takes precedence over
+        # PathToRegistryKey.
         #
         # .PARAMETER RefNameOfRegistryValue
         # Either this parameter or NameOfRegistryValue are required; if
-        # RefNameOfRegistryValue is specified, it is a reference to a string
-        # that contains the name of the registry value to be tested. If this
-        # parameter is not specified, then NameOfRegistryValue must be
-        # specified. If both are specified, then RefNameOfRegistryValue takes
-        # precedence over NameOfRegistryValue.
+        # RefNameOfRegistryValue is specified, it is a reference to a
+        # string that contains the name of the registry value to be tested.
+        # If this parameter is not specified, then NameOfRegistryValue must
+        # be specified. If both are specified, then RefNameOfRegistryValue
+        # takes precedence over NameOfRegistryValue.
         #
         # .PARAMETER NameOfRegistryValue
         # Either this parameter or RefNameOfRegistryValue are required; if
-        # NameOfRegistryValue is specified, it is a string that contains the
-        # name of the registry value to be tested. If this parameter is not
-        # specified, then RefNameOfRegistryValue must be specified. If both are
-        # specified, then RefNameOfRegistryValue takes precedence over
-        # NameOfRegistryValue.
+        # NameOfRegistryValue is specified, it is a string that contains
+        # the name of the registry value to be tested. If this parameter is
+        # not specified, then RefNameOfRegistryValue must be specified. If
+        # both are specified, then RefNameOfRegistryValue takes precedence
+        # over NameOfRegistryValue.
         #
         # .EXAMPLE
         # $strRegistryPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment'
@@ -156,41 +160,44 @@ function Get-WindowsOSProcessorArchitecture {
         #
         # .OUTPUTS
         # System.Boolean. Test-RegistryValue returns a boolean value
-        # indiciating whether the registry value exists. $true indicates the
-        # registry value exists; $false indicates the registry value does not
-        # exist, or that the specified registry key does not exist.
+        # indiciating whether the registry value exists. $true indicates
+        # the registry value exists; $false indicates the registry value
+        # does not exist, or that the specified registry key does not
+        # exist.
         #
         # .NOTES
-        # Oddly enough, it seems that passing string parameters by reference
-        # is not faster than passing them by value. I thought that passing by
-        # reference would be faster, but it seems that passing by value is
-        # faster. I don't know why this is the case, but it is.
+        # Oddly enough, it seems that passing string parameters by
+        # reference is not faster than passing them by value. I thought
+        # that passing by reference would be faster, but it seems that
+        # passing by value is faster. I don't know why this is the case,
+        # but it is.
         #
-        # Version: 1.0.20250406.0
+        # Version: 1.0.20250406.1
 
-        #region License ####################################################
+        #region License ################################################
         # Copyright (c) 2025 Frank Lesniak
         #
-        # Permission is hereby granted, free of charge, to any person obtaining
-        # a copy of this software and associated documentation files (the
-        # "Software"), to deal in the Software without restriction, including
-        # without limitation the rights to use, copy, modify, merge, publish,
-        # distribute, sublicense, and/or sell copies of the Software, and to
-        # permit persons to whom the Software is furnished to do so, subject to
-        # the following conditions:
+        # Permission is hereby granted, free of charge, to any person
+        # obtaining a copy of this software and associated documentation
+        # files (the "Software"), to deal in the Software without
+        # restriction, including without limitation the rights to use,
+        # copy, modify, merge, publish, distribute, sublicense, and/or sell
+        # copies of the Software, and to permit persons to whom the
+        # Software is furnished to do so, subject to the following
+        # conditions:
         #
         # The above copyright notice and this permission notice shall be
         # included in all copies or substantial portions of the Software.
         #
         # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-        # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-        # BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-        # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-        # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        # SOFTWARE.
-        #endregion License ####################################################
+        # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+        # OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+        # NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+        # HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+        # WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+        # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+        # OTHER DEALINGS IN THE SOFTWARE.
+        #endregion License ################################################
 
         param (
             [ref]$RefPathToRegistryKey = ([ref]$null),
@@ -206,8 +213,8 @@ function Get-WindowsOSProcessorArchitecture {
 
         # Specifying an empty string for the name of the registry value is
         # valid; it would mean the "default" value of the registry key.
-        # However, if both RefNameOfRegistryValue and NameOfRegistryValue are
-        # empty, then we have a problem.
+        # However, if both RefNameOfRegistryValue and NameOfRegistryValue
+        # are empty, then we have a problem.
         if (($null -eq $RefNameOfRegistryValue.Value) -and ($null -eq $NameOfRegistryValue)) {
             Write-Error -Message 'Either RefNameOfRegistryValue or NameOfRegistryValue must be specified.'
             return $false
